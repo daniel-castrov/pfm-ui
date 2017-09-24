8d33b5acbcc45091470cbc783d1b57fc4fe5198a
Gabrielle Campagna <gabycampagna@gmail.com>
2017-09-24 14:33:58 -0700

    View Program Identification components Swagger files

---
+++ src/app/generated/api/api.ts
@@ -0,0 +1,7 @@
+export * from './LabelApi';
+import { LabelApi }  from './LabelApi';
+export * from './LabelTypeApi';
+import { LabelTypeApi }  from './LabelTypeApi';
+export * from './ProgramApi';
+import { ProgramApi }  from './ProgramApi';
+export const APIS = [ LabelApi, LabelTypeApi, ProgramApi, ];
