8f033dd110be539bf5e21d753b66a7d27758ec1f
Gabrielle Campagna <gabycampagna@gmail.com>
2017-12-19 10:10:41 -0800

    Form updates

--- src/app/components/create-community/create-community.component.ts
+++ src/app/components/create-community/create-community.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 import { UserComponent } from '../user/user.component';
 import { Observable } from 'rxjs/Observable';
 import { } from '../../generated';
+import { FormsModule } from '@angular/forms'
 
 
 @Component({
@@ -11,6 +12,18 @@ import { } from '../../generated';
   styleUrls: ['./create-community.component.css']
 })
 export class CreateCommunityComponent implements OnInit {
+
+  community = {
+    communityname:''
+}
+
+onSubmit({value, valid}){
+ if(valid){
+     console.log(value);
+ } else {
+     console.log('Form is invalid');
+ }
+}
 
   constructor() { }
 
