504386ab9826f37c49c5a64512652f172b8101cb
Gabrielle Campagna <gabycampagna@gmail.com>
2018-01-24 09:56:48 -0800

    Create Community Details Component. Add Approver User form to Create Community page. Slide down accordion panels.

---
+++ src/app/components/community-details/community-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
+@Component({
+  selector: 'app-community-details',
+  templateUrl: './community-details.component.html',
+  styleUrls: ['./community-details.component.css']
+})
+export class CommunityDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
