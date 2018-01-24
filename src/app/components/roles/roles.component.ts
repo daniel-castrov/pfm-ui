404fb4c67d53f45af1f60b27188d0490f5673292
Gabrielle Campagna <gabycampagna@gmail.com>
2018-01-24 09:06:01 -0800

    Add Move Item jQuery feature to Roles component

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -2,17 +2,86 @@ import { Component, OnInit } from '@angular/core';
 import { PickListModule } from 'primeng/picklist';
 import { AccordionModule } from 'primeng/accordion';
 import { MenuItem } from 'primeng/api';
-
+import * as $ from 'jquery/dist/jquery.min.js';
 
 @Component({
   selector: 'app-roles',
   templateUrl: './roles.component.html',
   styleUrls: ['./roles.component.css']
 })
-export class RolesComponent  {
 
+export class RolesComponent {
   status: any = {
-     isFirstOpen: true,
-     isFirstDisabled: false
-   };
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+
+  public ngOnInit() {
+
+    $(document).ready(function() {
+      $('#btn-add').click(function() {
+        $('#select-from option:selected').each(function() {
+          $('#select-to').append("<option value='" + $(this).val() + "'>" + $(this).text() + "</option>");
+          $(this).remove();
+        });
+      });
+      $('#btn-remove').click(function() {
+        $('#select-to option:selected').each(function() {
+          $('#select-from').append("<option value='" + $(this).val() + "'>" + $(this).text() + "</option>");
+          $(this).remove();
+        });
+      });
+      $('#btn-up').bind('click', function() {
+        $('#select-to option:selected').each(function() {
+          var newPos = $('#select-to option').index(this) - 1;
+          if (newPos > -1) {
+            $('#select-to option').eq(newPos).before("<option value='" + $(this).val() + "' selected='selected'>" + $(this).text() + "</option>");
+            $(this).remove();
+          }
+        });
+      });
+      $('#btn-down').bind('click', function() {
+        var countOptions = $('#select-to option').size();
+        $('#select-to option:selected').each(function() {
+          var newPos = $('#select-to option').index(this) + 1;
+          if (newPos < countOptions) {
+            $('#select-to option').eq(newPos).after("<option value='" + $(this).val() + "' selected='selected'>" + $(this).text() + "</option>");
+            $(this).remove();
+          }
+        });
+      });
+      $('#btn-up-source').bind('click', function() {
+        $('#select-from option:selected').each(function() {
+          var newPos = $('#select-from option').index(this) - 1;
+          if (newPos > -1) {
+            $('#select-from option').eq(newPos).before("<option value='" + $(this).val() + "' selected='selected'>" + $(this).text() + "</option>");
+            $(this).remove();
+          }
+        });
+      });
+      $('#btn-down-source').bind('click', function() {
+        var countOptions = $('#select-from option').size();
+        $('#select-from option:selected').each(function() {
+          var newPos = $('#select-from option').index(this) + 1;
+          if (newPos < countOptions) {
+            $('#select-from option').eq(newPos).after("<option value='" + $(this).val() + "' selected='selected'>" + $(this).text() + "</option>");
+            $(this).remove();
+          }
+        });
+      });
+
+      $('#multi').submit(function() {
+        var selectalla = $('#select-to').val();
+        if (!selectalla) {
+          $('#select-to option').attr('selected', 'selected');
+        }
+        var selectallb = $('#select-from').val();
+        if (!selectallb) {
+          $('#select-from option').attr('selected', 'selected');
+        }
+
+      });
+
+    });
   }
+}
