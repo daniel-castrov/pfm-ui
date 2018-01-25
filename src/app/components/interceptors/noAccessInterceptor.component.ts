6d5fcf74dee65043a0348b70b71fd14900ca789f
Michael Giordano <michael.giordano@p-exchange.com>
2018-01-25 11:48:20 -0500

    Added 500 response to the interceptor

--- src/app/components/interceptors/noAccessInterceptor.component.ts
+++ src/app/components/interceptors/noAccessInterceptor.component.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
-import { Observable }                                        from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
 
 
@@ -13,12 +13,18 @@ export class NoAccessInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).do(event => {}, err => {
-      if (err instanceof HttpErrorResponse && err.status == 403) {
-          // handle 403 errors
+      
+      if (err instanceof HttpErrorResponse ) {
+        // handle 403 errors      
+        if ( err.status == 403) {
           this.router.navigate(['/no-access']);
+        }
+        // handle 500 errors      
+        if ( err.status == 500) {
+          this.router.navigate(['/no-access']);
+        }
+        // Handle more errors
       }
-      // Handle more errors
-
       // If you do nothing then this interceptor just passes the requests and reponses through
   });
   }
