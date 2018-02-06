67b484a706369079b88d11ee4331db177842e67b
Michael Giordano <michael.giordano@p-exchange.com>
2018-02-06 18:28:49 -0500

    Initial Assign User To Role done.

--- src/app/components/manage-users/manage-users.component.ts
+++ src/app/components/manage-users/manage-users.component.ts
@@ -6,7 +6,6 @@ import { Communication } from '../../generated/model/communication';
 import { HeaderComponent } from '../../components/header/header.component';
 import { CommunityService, Community, UserService } from '../../generated';
 import { Role, RoleService, UserRole } from '../../generated';
-
 import { Response, ResponseContentType } from '@angular/http';
 import {
   HttpClient, HttpHeaders, HttpParams,
@@ -24,18 +23,28 @@ export class ManageUsersComponent implements OnInit {
 
   @ViewChild(HeaderComponent) header;
 
+  // This is the id of the user we are interested in ... the targetUser
   id: string;
-  jsonUser: string;
 
+  // The user we are interested in.
   targetUser:User;
 
-  targetUserRoles:Role[]=[];
+  // all the roles available ( all minus the ones we have )
   allRoles:Role[]=[];
-  userRoles:UserRole[]=[];
 
+  // The roles that this user has
+  userRoles:UserRole[]=[];
+  
+  // The name of the role we are assigning the targetUser to
+  addedrole:string;
+
+  // an original copy of the targted user so we can roll back.
   reftargetUser:User;
+
+  // Have we hit the edit button or are we just viewing?
   isdEditMode=false;
 
+  // Any error from a rest service
   resultError: string;
 
   constructor(
@@ -52,9 +61,9 @@ export class ManageUsersComponent implements OnInit {
 
   ngOnInit() {
     this.getTargetUser();
-    this.getRoles();
+    this.getAllRoles();
+    this.getTargetedUserRoles();
   }
-
 
   getTargetUser(): void{
     let result:RestResult;
@@ -63,18 +72,13 @@ export class ManageUsersComponent implements OnInit {
       result = c;
       this.resultError = result.error;
       this.targetUser=result.result;
-      
-      console.log(this.targetUser);
-      
+      console.log(this.targetUser.firstName);
        // Make a copy of the current user so we can revert changes
        this.reftargetUser = JSON.parse(JSON.stringify(this.targetUser));
-
-
     });
   }
 
   saveTargetUser():void{
-
     // clean up empty communications
     for(var i = this.targetUser.communications.length - 1; i >= 0; i--) {
       if(this.targetUser.communications[i].value === "") {
@@ -82,17 +86,17 @@ export class ManageUsersComponent implements OnInit {
       }
     }
     let result:RestResult;
-    //this.userService.create(this.targetUser)
-    //.subscribe(r => {
-    //  result=r;
-    //});
-
+    this.userService.create(this.targetUser)
+    .subscribe(r => {
+      result=r;
+    });
     this.isdEditMode=false;
   }
 
   // toggle edit mode
   editMode():void{
     this.isdEditMode=true;
+    this.buildAvailableRoles();
   }
 
   cancelEdit():void{
@@ -100,6 +104,7 @@ export class ManageUsersComponent implements OnInit {
     this.targetUser = JSON.parse(JSON.stringify(this.reftargetUser));
     this.isdEditMode=false;
   }
+
   createNewCommunication():void{
     let newCom:Communication;
     newCom = new Object;
@@ -112,39 +117,76 @@ export class ManageUsersComponent implements OnInit {
   }
 
 
-  getRoles(): void{
+  getAllRoles(): void{
     
-    let result:RestResult;
+    let result1:RestResult;
     this.roleService.findall()
     .subscribe(c => {
-      result = c;
-      this.resultError = result.error;
-      this.allRoles=result.result;
+      result1 = c;
+      this.resultError = result1.error;
+      this.allRoles=result1.result;
       let role:Role;
-    console.log("All Roles");
-    for ( role of this.allRoles ){
-      console.log(role.roleName);
-    }
     });
-    
-    this.roleService.find(this.id)
-    .subscribe(c => {
-      result = c;
-      this.resultError = result.error;
-      this.userRoles=result.result;
-
-      let rolee:Role;
-      console.log("My Roles");
-      for ( rolee of this.userRoles ){
-        console.log(rolee.roleName);
-      }
-  
-    });
-
-
-
   }
 
+  getTargetedUserRoles(): void{
+
+    let result2:RestResult;
+    this.roleService.find(this.id)
+    .subscribe(c => {
+      result2 = c;
+      this.resultError = result2.error;
+      this.userRoles=result2.result;
+//      let role:Role;
+//      console.log("My Roles");
+//      for ( role of this.userRoles ){
+//        console.log(role.roleName);
+//      }
+      
+    });
+  }
+
+buildAvailableRoles():void{
+  console.log("MATCH");
+  let userRole:UserRole;
+  for ( userRole of this.userRoles ){
+    let role:Role;
+    for ( role of this.allRoles ){
+      if ( userRole.roleName === role.roleName ){
+        var index = this.allRoles.indexOf(role);
+        if ( index > -1 ){
+          this.allRoles.splice(index, 1);
+        }
+      }
+    }
+  }
+
+}
+
+  addRole(): void{
+    console.log("addRole"+this.addedrole);
+
+    let newUsreRole:UserRole;
+    newUsreRole = new Object();
+
+    newUsreRole.email=this.targetUser.email;
+    newUsreRole.roleName=this.addedrole;
+
+    let reuslt3:RestResult; 
+    
+    this.roleService.assignUserToRole(newUsreRole)
+    .subscribe(c => {
+      reuslt3 = c;
+      this.resultError = reuslt3.error;
+      this.userRoles.push(newUsreRole);
+      this.resetAddRole();
+      this.buildAvailableRoles();
+    });
+  }
+  
+  resetAddRole():void{
+    this.addedrole='';
+  }
 
 
 }
