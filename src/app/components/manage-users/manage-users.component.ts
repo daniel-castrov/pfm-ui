aaaa37615e385dae542253b681317e4e971a09e4
Gabrielle Campagna <gabycampagna@gmail.com>
2018-02-05 16:42:42 -0800

    Add jsonUser to test data

--- src/app/components/manage-users/manage-users.component.ts
+++ src/app/components/manage-users/manage-users.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserComponent } from '../user/user.component';
-import { HeaderComponent } from '../../components/header/header.component';
 import { User } from '../../generated/model/user';
-import { NgForOf } from '@angular/common/src/directives';
-import { AccordionModule } from 'primeng/accordion';
-
+import { RestResult } from '../../generated/model/restResult';
+import { Communication } from '../../generated/model/communication';
+import { GrantedAuthority } from '../../generated/model/grantedAuthority';
+import { HeaderComponent } from '../../components/header/header.component';
+import { BlankService } from '../../generated/api/blank.service';
+import { Response, ResponseContentType } from '@angular/http';
+import {
+  HttpClient, HttpHeaders, HttpParams,
+  HttpResponse, HttpEvent
+} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-manage-users',
@@ -15,14 +23,35 @@ export class ManageUsersComponent implements OnInit {
 
   @ViewChild(HeaderComponent) header;
 
-  currentusername: string;
-  currentUser: User;
-  refcurrentUser: User;
+  id: number;
+  jsonUser: string;
 
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private blankService: BlankService,
+  ) {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params.id;
+    });
+  }
 
   ngOnInit() {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    //console.log("getCurrentUser");
+
+    let resp = this.blankService.blank("response", true);
+
+    resp.subscribe(
+      (r: HttpResponse<RestResult>) => {
+        var authHeader = r.headers.get('Authorization');
+        this.jsonUser = atob(authHeader);
+      }
+
+    );
   }
 
 }
