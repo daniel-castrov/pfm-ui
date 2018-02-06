de7738ad39058a0801a67c6e860924cb3042b2e5
Gabrielle Campagna <gabycampagna@gmail.com>
2018-02-06 15:31:35 -0800

    Update roles component

--- src/app/components/access-change/access-change.component.ts
+++ src/app/components/access-change/access-change.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
-import { PickListModule } from 'primeng/picklist';
-import { MenuItem } from 'primeng/api';
-import * as $ from 'jquery/dist/jquery.min.js';
+import * as $ from 'jquery/dist/jquery.slim.min.js';
 
 
 @Component({
@@ -22,68 +20,68 @@ export class AccessChangeComponent implements OnInit {
     public ngOnInit() {
 
       $(document).ready(function() {
-        $('#btn-add').click(function() {
-          $('#select-from option:selected').each(function() {
-            $('#select-to').append("<option value='" + $(this).val() + "'>" + $(this).text() + "</option>");
-            $(this).remove();
+          $('#btn-add').click(function(){
+              $('#select-from option:selected').each( function() {
+                      $('#select-to').append("<option value='"+$(this).val()+"'>"+$(this).text()+"</option>");
+                  $(this).remove();
+              });
           });
-        });
-        $('#btn-remove').click(function() {
-          $('#select-to option:selected').each(function() {
-            $('#select-from').append("<option value='" + $(this).val() + "'>" + $(this).text() + "</option>");
-            $(this).remove();
+          $('#btn-remove').click(function(){
+              $('#select-to option:selected').each( function() {
+                  $('#select-from').append("<option value='"+$(this).val()+"'>"+$(this).text()+"</option>");
+                  $(this).remove();
+              });
           });
-        });
-        $('#btn-up').bind('click', function() {
-          $('#select-to option:selected').each(function() {
-            var newPos = $('#select-to option').index(this) - 1;
-            if (newPos > -1) {
-              $('#select-to option').eq(newPos).before("<option value='" + $(this).val() + "' selected='selected'>" + $(this).text() + "</option>");
-              $(this).remove();
-            }
+          $('#btn-up').bind('click', function() {
+              $('#select-to option:selected').each( function() {
+                  var newPos = $('#select-to option').index(this) - 1;
+                  if (newPos > -1) {
+                      $('#select-to option').eq(newPos).before("<option value='"+$(this).val()+"' selected='selected'>"+$(this).text()+"</option>");
+                      $(this).remove();
+                  }
+              });
           });
-        });
-        $('#btn-down').bind('click', function() {
-          var countOptions = $('#select-to option').size();
-          $('#select-to option:selected').each(function() {
-            var newPos = $('#select-to option').index(this) + 1;
-            if (newPos < countOptions) {
-              $('#select-to option').eq(newPos).after("<option value='" + $(this).val() + "' selected='selected'>" + $(this).text() + "</option>");
-              $(this).remove();
-            }
+          $('#btn-down').bind('click', function() {
+              var countOptions = $('#select-to option').size();
+              $('#select-to option:selected').each( function() {
+                  var newPos = $('#select-to option').index(this) + 1;
+                  if (newPos < countOptions) {
+                      $('#select-to option').eq(newPos).after("<option value='"+$(this).val()+"' selected='selected'>"+$(this).text()+"</option>");
+                      $(this).remove();
+                  }
+              });
           });
-        });
-        $('#btn-up-source').bind('click', function() {
-          $('#select-from option:selected').each(function() {
-            var newPos = $('#select-from option').index(this) - 1;
-            if (newPos > -1) {
-              $('#select-from option').eq(newPos).before("<option value='" + $(this).val() + "' selected='selected'>" + $(this).text() + "</option>");
-              $(this).remove();
-            }
+          $('#btn-up-source').bind('click', function() {
+              $('#select-from option:selected').each( function() {
+                  var newPos = $('#select-from option').index(this) - 1;
+                  if (newPos > -1) {
+                      $('#select-from option').eq(newPos).before("<option value='"+$(this).val()+"' selected='selected'>"+$(this).text()+"</option>");
+                      $(this).remove();
+                  }
+              });
           });
-        });
-        $('#btn-down-source').bind('click', function() {
-          var countOptions = $('#select-from option').size();
-          $('#select-from option:selected').each(function() {
-            var newPos = $('#select-from option').index(this) + 1;
-            if (newPos < countOptions) {
-              $('#select-from option').eq(newPos).after("<option value='" + $(this).val() + "' selected='selected'>" + $(this).text() + "</option>");
-              $(this).remove();
-            }
+          $('#btn-down-source').bind('click', function() {
+              var countOptions = $('#select-from option').size();
+              $('#select-from option:selected').each( function() {
+                  var newPos = $('#select-from option').index(this) + 1;
+                  if (newPos < countOptions) {
+                      $('#select-from option').eq(newPos).after("<option value='"+$(this).val()+"' selected='selected'>"+$(this).text()+"</option>");
+                      $(this).remove();
+                  }
+              });
           });
-        });
 
-        $('#multi').submit(function() {
-          var selectalla = $('#select-to').val();
-          if (!selectalla) {
-            $('#select-to option').attr('selected', 'selected');
-          }
-          var selectallb = $('#select-from').val();
-          if (!selectallb) {
-            $('#select-from option').attr('selected', 'selected');
-          }
+          $('#multi').submit( function () {
+              var selectalla = $('#select-to').val();
+              if(!selectalla) {
+                  $('#select-to option').attr('selected','selected');
+              }
+              var selectallb = $('#select-from').val();
+              if(!selectallb) {
+                  $('#select-from option').attr('selected','selected');
+              }
 
-        });
+          });
 
       });
     }
