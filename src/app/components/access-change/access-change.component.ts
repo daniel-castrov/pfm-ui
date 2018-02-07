680f1800c849a7ca537ce8da6537e1c205cbd842
Gabrielle Campagna <gabycampagna@gmail.com>
2018-02-07 11:17:05 -0800

    Update feature with new multi select library for jQuery

--- src/app/components/access-change/access-change.component.ts
+++ src/app/components/access-change/access-change.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from '../../components/header/header.component';
 import * as $ from 'jquery';
 
 declare const $: any;
+declare const jQuery: any;
 
 @Component({
   selector: 'app-access-change',
@@ -12,75 +13,13 @@ declare const $: any;
 
 export class AccessChangeComponent implements OnInit {
 
-
   @ViewChild(HeaderComponent) header;
 
     public ngOnInit() {
 
-      $(document).ready(function() {
-          $('#btn-add').click(function(){
-              $('#select-from option:selected').each( function() {
-                      $('#select-to').append("<option value='"+$(this).val()+"'>"+$(this).text()+"</option>");
-                  $(this).remove();
-              });
-          });
-          $('#btn-remove').click(function(){
-              $('#select-to option:selected').each( function() {
-                  $('#select-from').append("<option value='"+$(this).val()+"'>"+$(this).text()+"</option>");
-                  $(this).remove();
-              });
-          });
-          $('#btn-up').bind('click', function() {
-              $('#select-to option:selected').each( function() {
-                  var newPos = $('#select-to option').index(this) - 1;
-                  if (newPos > -1) {
-                      $('#select-to option').eq(newPos).before("<option value='"+$(this).val()+"' selected='selected'>"+$(this).text()+"</option>");
-                      $(this).remove();
-                  }
-              });
-          });
-          $('#btn-down').bind('click', function() {
-              var countOptions = $('#select-to option').size();
-              $('#select-to option:selected').each( function() {
-                  var newPos = $('#select-to option').index(this) + 1;
-                  if (newPos < countOptions) {
-                      $('#select-to option').eq(newPos).after("<option value='"+$(this).val()+"' selected='selected'>"+$(this).text()+"</option>");
-                      $(this).remove();
-                  }
-              });
-          });
-          $('#btn-up-source').bind('click', function() {
-              $('#select-from option:selected').each( function() {
-                  var newPos = $('#select-from option').index(this) - 1;
-                  if (newPos > -1) {
-                      $('#select-from option').eq(newPos).before("<option value='"+$(this).val()+"' selected='selected'>"+$(this).text()+"</option>");
-                      $(this).remove();
-                  }
-              });
-          });
-          $('#btn-down-source').bind('click', function() {
-              var countOptions = $('#select-from option').size();
-              $('#select-from option:selected').each( function() {
-                  var newPos = $('#select-from option').index(this) + 1;
-                  if (newPos < countOptions) {
-                      $('#select-from option').eq(newPos).after("<option value='"+$(this).val()+"' selected='selected'>"+$(this).text()+"</option>");
-                      $(this).remove();
-                  }
-              });
-          });
-
-          $('#multi').submit( function () {
-              var selectalla = $('#select-to').val();
-              if(!selectalla) {
-                  $('#select-to option').attr('selected','selected');
-              }
-              var selectallb = $('#select-from').val();
-              if(!selectallb) {
-                  $('#select-from option').attr('selected','selected');
-              }
-
-          });
-
+      jQuery(document).ready(function($) {
+        $('#multiselect1').multiselect();
+        $('#multiselect2').multiselect();
       });
 
     }
