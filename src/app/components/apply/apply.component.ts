9c7f0dc3d8da66ce407e9854fab05569ceb96448
Gabrielle Campagna <gabycampagna@gmail.com>
2018-02-15 21:29:00 -0800

    Update to form.  Dropdown fields are dynamic. Checkbox added to NDA. Marker added nest to ‘Sign NDA’. Clean up.

--- src/app/components/apply/apply.component.ts
+++ src/app/components/apply/apply.component.ts
@@ -7,22 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplyComponent implements OnInit {
 
-    apply = {
-      firstname:'',
-      middlename:'',
-      lastname:'',
-      rank:'',
-      job:'',
-      email:'',
-      phone:'',
-      address:'',
-      organization:'',
-      orgrequest:'',
-      branch:'',
-      sponsorname:'',
-      sponsoremail:'',
-      sponsorphone:''
+  apply = {
+    firstname:'',
+    middlename:'',
+    lastname:'',
+    rank:'',
+    job:'',
+    email:'',
+    phone:'',
+    address:'',
+    organization:'',
+    orgrequest:'',
+    sponsorname:'',
+    sponsoremail:'',
+    sponsorphone:''
   }
+
+  branches = [
+    { id: 1, name: 'Army (USA)' },
+    { id: 2, name: 'USAF (Air Force)' },
+    { id: 3, name: 'USN (Navy)' },
+    { id: 4, name: 'USMC (Marine Corps)' },
+    { id: 5, name: 'USCG (Coast Guard)' },
+    { id: 6, name: 'Other' },
+    { id: 7, name: 'None)' }
+  ];
+
+  communities = [
+    { id: 1, name: 'JPEO-CBO' },
+    { id: 2, name: 'JSTO-CBD' },
+    { id: 3, name: 'CBDP' },
+    { id: 4, name: 'TRAC-CWMD' },
+    { id: 5, name: 'TRAC-CDP' },
+    { id: 6, name: 'TRAC-CDP' },
+    { id: 7, name: 'NM)' },
+    { id: 8, name: 'DTRA' }
+  ];
 
   onSubmit({value, valid}){
    if(valid){
@@ -32,6 +52,9 @@ export class ApplyComponent implements OnInit {
    }
   }
 
+  submit(f){
+    f.value
+  }
     constructor() { }
 
     ngOnInit() {
