b652bb4e7b28a2b61ba93e570006dfc39162e2fd
Ry Bobko <ryan.bobko@p-exchange.com>
2018-04-02 12:43:20 -0400

    resolved program filter inconsistencies

--- src/app/components/programs/program-view/program-view.component.ts
+++ src/app/components/programs/program-view/program-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, Input } from '@angular/core';
-import { ProgramsService, Program } from '../../../generated';
+import { ProgramsService, Program, ProgramFilter } from '../../../generated';
 import * as $ from 'jquery';
 
 // Other Components
@@ -18,19 +18,41 @@ export class ProgramViewComponent implements OnInit {
   @ViewChild(HeaderComponent) header;
   private allprograms: Program[] = [];
   private current: Program = null;
-  startyear: number = 2013;
+  private startyear: number = 2013;
 
+  private appropriations: string[] = [];
+  private blins: string[] = [];
+  private opagencies: string[] = [];
 
-  constructor(private programs: ProgramsService) {
+  constructor(private programs: ProgramsService ) {
   }
 
   ngOnInit() {
-    this.programs.getall().subscribe(
+    this.programs.getAll().subscribe(
       (data) => {
         this.allprograms = data.result;
         this.current = this.allprograms[0];
       }
     );
-  }
 
+    this.programs.getTagsByType("Appropriation").subscribe(
+      (data) => { this.appropriations = data.result;}
+    );    
+    this.programs.getTagsByType("Op_Agency").subscribe(
+      (data) => { this.opagencies = data.result; }
+    );
+    this.programs.getSearchBlins().subscribe(
+      (data) => { this.blins = data.result; }
+    );
+
+
+    var pf: ProgramFilter = {
+      agency: '6A',
+      appropriation: 'O&M'
+    };
+    this.programs.search(pf).subscribe(
+      (data) => {
+        console.log(data.result);
+      } );
+  }
 }
