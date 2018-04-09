eb1552886eda4f2142968e750f9a3bc880a337cf
ry99 <ryan@ostrich-emulators.com>
2018-04-09 13:45:50 -0400

    made program view reference user-friendly names instead of abbreviations updated loader module to load from new spreadsheet added BSV_Strategy to known tags list

--- src/app/components/programs/program-view/program.component.ts
+++ src/app/components/programs/program-view/program.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Program } from '../../../generated';
+import { Program, ProgramsService, Tag } from '../../../generated';
 
 @Component({
   selector: 'program',
@@ -10,9 +10,25 @@ export class ProgramComponent implements OnInit {
   @Input() current: Program;
   @Input() startyear: number;
 
-  constructor() {
+  private tagnames: Map<string, Map<string,string>> = new Map<string, Map<string,string>>();
+
+  constructor(private progsvc: ProgramsService) {
   }
 
   ngOnInit() {
+    var my: ProgramComponent = this;
+    this.progsvc.getSearchTags().subscribe(tagtypes => { 
+      tagtypes.result.forEach(function (tagtype: string) {
+        
+        var map: Map<string, string> = new Map<string, string>();
+        my.progsvc.getTagsByType(tagtype).subscribe(data => {
+          data.result.forEach(function (x: Tag) {
+            map.set(x.abbr, x.name);
+          });
+        });
+
+        my.tagnames.set(tagtype, map);
+      });
+    });
   }
 }
