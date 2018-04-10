f551e50315154c9e99275a6248eadbebb0d687f2
Gabrielle Campagna <gabycampagna@gmail.com>
2018-04-10 08:38:17 -0700

    Remove jQuery filter for table

--- src/app/components/user-management/approve-requests/approve-requests.component.ts
+++ src/app/components/user-management/approve-requests/approve-requests.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit, ViewChild, Input } from '@angular/core';
-import * as $ from 'jquery';
 
 // Other Components
 import { HeaderComponent } from '../../../components/header/header.component';
-
-declare const $: any;
-declare const jQuery: any;
 
 @Component({
   selector: 'approve-requests',
@@ -20,47 +16,5 @@ export class ApproveRequestsComponent implements OnInit {
 
   ngOnInit() {
 
-    $(document).ready(function(){
-    $('.filterable .btn-filter').click(function(){
-        var $panel = $(this).parents('.filterable'),
-        $filters = $panel.find('.filters input'),
-        $tbody = $panel.find('.table tbody');
-        if ($filters.prop('disabled') == true) {
-            $filters.prop('disabled', false);
-            $filters.first().focus();
-        } else {
-            $filters.val('').prop('disabled', true);
-            $tbody.find('.no-result').remove();
-            $tbody.find('tr').show();
-        }
-    });
-
-        $('.filterable .filters input').keyup(function(e){
-          /* Ignore tab key */
-          var code = e.keyCode || e.which;
-          if (code == '9') return;
-          /* Useful DOM data and selectors */
-          var $input = $(this),
-          inputContent = $input.val().toLowerCase(),
-          $panel = $input.parents('.filterable'),
-          column = $panel.find('.filters th').index($input.parents('th')),
-          $table = $panel.find('.table'),
-          $rows = $table.find('tbody tr');
-          /* Dirtiest filter function ever ;) */
-          var $filteredRows = $rows.filter(function(){
-              var value = $(this).find('td').eq(column).text().toLowerCase();
-              return value.indexOf(inputContent) === -1;
-          });
-          /* Clean previous no-result if exist */
-          $table.find('tbody .no-result').remove();
-          /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
-          $rows.show();
-          $filteredRows.hide();
-          /* Prepend no-result row if all rows are filtered */
-          if ($filteredRows.length === $rows.length) {
-              $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
-          }
-      });
-    });
   }
 }
