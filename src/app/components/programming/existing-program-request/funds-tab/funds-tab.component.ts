a9414ac59f63eba6e58ab136c578069c8a1a3f53
Gabrielle Campagna <gabycampagna@gmail.com>
2018-05-02 17:02:48 -0700

    Add editable cells to tables

--- src/app/components/programming/existing-program-request/funds-tab/funds-tab.component.ts
+++ src/app/components/programming/existing-program-request/funds-tab/funds-tab.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
+
+declare const $: any;
+declare const jQuery: any;
+
 @Component({
   selector: 'funds-tab',
   templateUrl: './funds-tab.component.html',
@@ -10,6 +15,63 @@ export class FundsTabComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+        //jQuery for editing table
+        var $TABLE = $('#pr-funds', '#pr-funding-line');
+        var $BTN = $('#export-btn');
+        var $EXPORT = $('#export');
+
+        $('.table-add').click(function () {
+          var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
+          $TABLE.find('table').append($clone);
+        });
+
+        $('.table-remove').click(function () {
+          $(this).parents('tr').detach();
+        });
+
+        $('.table-up').click(function () {
+          var $row = $(this).parents('tr');
+          if ($row.index() === 1) return; // Don't go above the header
+          $row.prev().before($row.get(0));
+        });
+
+        $('.table-down').click(function () {
+          var $row = $(this).parents('tr');
+          $row.next().after($row.get(0));
+        });
+
+        // A few jQuery helpers for exporting only
+        jQuery.fn.pop = [].pop;
+        jQuery.fn.shift = [].shift;
+
+        $BTN.click(function () {
+          var $rows = $TABLE.find('tr:not(:hidden)');
+          var headers = [];
+          var data = [];
+
+          // Get the headers (add special header logic here)
+          $($rows.shift()).find('th:not(:empty)').each(function () {
+            headers.push($(this).text().toLowerCase());
+          });
+
+          // Turn all existing rows into a loopable array
+          $rows.each(function () {
+            var $td = $(this).find('td');
+            var h = {};
+
+            // Use the headers from earlier to name our hash keys
+            headers.forEach(function (header, i) {
+              h[header] = $td.eq(i).text();
+            });
+
+            data.push(h);
+          });
+
+          // Output the result
+          $EXPORT.text(JSON.stringify(data));
+        });
+
   }
 
 }
