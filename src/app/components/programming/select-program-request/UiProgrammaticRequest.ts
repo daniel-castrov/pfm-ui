aecdd42aefa982088e99f7920a37d2f7547fc0f1
Rossen <rossen.nedeltchev@p-exchange.com>
2018-05-24 13:30:56 -0400

    Populated the Subprogram and Variant columns

--- src/app/components/programming/select-program-request/UiProgrammaticRequest.ts
+++ src/app/components/programming/select-program-request/UiProgrammaticRequest.ts
@@ -4,12 +4,22 @@ import { FundingLine } from '../../../generated/model/fundingLine';
 export class UiProgrammaticRequest {
   constructor(public programmaticRequest: ProgrammaticRequest) {}
 
-  get id():string {return this.programmaticRequest.id;}
-  get state():string {return this.programmaticRequest.state;}
-  get shortName():string {return this.programmaticRequest.shortName;}
-  get fundingLines():FundingLine[] {return this.programmaticRequest.fundingLines;}
+  get id():string {return this.programmaticRequest.id}
+  get state():string {return this.programmaticRequest.state}
+  get shortName():string {return this.programmaticRequest.shortName}
+  get fundingLines():FundingLine[] {return this.programmaticRequest.fundingLines}
+  get parentId():string {return this.programmaticRequest.parentId}
   getToa(year:number): any {
       const sum = this.programmaticRequest.fundingLines.map( fundingLine=>fundingLine.funds[year] ).reduce((a,b)=>a+b, 0);
       return isNaN(sum) ? '' : sum;
+  }
+  get isSubprogram(): boolean {
+    return this.programmaticRequest.parentId !== null;
+  }
+  get isNonVariantSubprogram(): boolean {
+    return this.isSubprogram && this.programmaticRequest.type !== 'VARIANT';
+  }
+  get isVariantSubprogram(): boolean {
+    return this.isSubprogram && this.programmaticRequest.type === 'VARIANT';
   }
 }
