3e7abb3832a98d3382689b1c1b848e4c19075afb
Rossen <rossen.nedeltchev@p-exchange.com>
2018-05-30 22:57:53 -0400

    partial implementation of PR creation

--- src/app/components/programming/select-program-request/new-programmatic-request/new-programmatic-request.component.ts
+++ src/app/components/programming/select-program-request/new-programmatic-request/new-programmatic-request.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProgrammaticRequest } from './../../../../generated/model/programmaticRequest';
+import { PRService } from './../../../../generated/api/pR.service';
+import { Component, Input } from '@angular/core';
 
 // Other Components
 import { Pom } from '../../../../generated/model/pom';
@@ -10,4 +13,31 @@ import { Pom } from '../../../../generated/model/pom';
 })
 export class NewProgrammaticRequestComponent {
 
+  radio: string;
+  @Input() pom: Pom;
+
+  constructor(
+    private prService: PRService,
+    private router: Router
+  ) {}
+
+  async next() {
+    switch(this.radio) {
+      case 'ExistingProgram':
+      case 'NewSubprogram':
+        break;
+      case 'NewProgram':
+        const pr: ProgrammaticRequest = (await this.prService.create(this.createPR()).toPromise()).result;
+        this.router.navigate(['/existing-program-request', pr.id])
+    }
+  }
+
+  private createPR() {
+    const pr: ProgrammaticRequest = new Object();
+    pr.phaseId = this.pom.id;
+    pr.bulkOrigin = false;
+    pr.state = 'OUTSTANDING';
+    pr.type = 'PROGRAM';
+    return pr;
+  }
 }
