4367a46e7fdd59ad3d4b824e9406d8a2e925148b
Michael Giordano <michael.giordano@p-exchange.com>
2018-06-18 17:33:59 -0400

    Added a radio button on the Create POM page for getting EPP data (or not)

--- src/app/components/programming/create-pom-session/create-pom-session.component.ts
+++ src/app/components/programming/create-pom-session/create-pom-session.component.ts
@@ -38,6 +38,8 @@ export class CreatePomSessionComponent implements OnInit {
   private tooMuchToa: boolean = false;
   private orgsums: Map<string, number> = new Map<string, number>();
   private yeardiffs: Map<number, number> = new Map<number, number>();
+  
+  private useEpp = false;
 
   constructor(private detailsvc: MyDetailsService, private communityService: CommunityService,
     private orgsvc: OrganizationService, private pomsvc: POMService, private pbsvc: PBService,
@@ -239,6 +241,26 @@ export class CreatePomSessionComponent implements OnInit {
 
   }
 
+  loadFY4fromEpp(){
+
+    if ( this.useEpp==true ) {
+      // start dummy code 
+      // Replace with calls to get actual EPP data
+      var n = this.orgs.length;
+      var t=125;
+      this.orgs.forEach( org => {
+      this.orgtoas.get(org.id).set(this.fy+4,t);
+      this.toas.set(this.fy+4, n*t);
+      }); // end dummy code
+    } else {
+      // replace any values in fy+4 with 0
+      this.orgs.forEach( org => {
+      this.orgtoas.get(org.id).set(this.fy+4,0);
+      this.toas.set(this.fy+4, 0);
+      });
+    }
+    this.resetTotals();
+  }
 
   submit() {
     var my: CreatePomSessionComponent = this;
