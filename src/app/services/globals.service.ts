836b0c117c3f9fb0441c35d00dbd6abce48715de
Rossen <rossen.nedeltchev@p-exchange.com>
2018-06-22 21:43:29 -0400

    refactored and made Appropriations, Blins and OpAgencies use Tags

--- src/app/services/globals.service.ts
+++ src/app/services/globals.service.ts
@@ -4,15 +4,40 @@ import { RestResult } from './../generated/model/restResult';
 import { MyDetailsService } from './../generated/api/myDetails.service';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map'
+import { ProgramsService } from '../generated/api/programs.service';
+import { Tag } from '../generated/model/tag';
 
 @Injectable()
 export class GlobalsService {
 
-  constructor(private myDetailsService: MyDetailsService) {}
+  constructor(private myDetailsService: MyDetailsService,
+              private programsService: ProgramsService) {}
 
   // TODO: make it cache
   user(): Observable<User> {
     return this.myDetailsService.getCurrentUser().map( (response: RestResult) => response.result );
   }
+
+  private tagAbbreviations(type: string): Promise<string[]> {
+    return this.programsService.getTagsByType(type)
+            .map((result: RestResult) => result.result)
+            .map((tags: Tag[]) => tags.map((tag:Tag)=>tag.abbr))
+            .map((tags: string[]) => tags.sort())
+            .toPromise()
+  }
+
+  tagAbbreviationsForOpAgency(): Promise<string[]> {
+    return this.tagAbbreviations('OpAgency (OA)');
+  }
+
+  tagAbbreviationsForAppropriation(): Promise<string[]> {
+    return this.tagAbbreviations('Appropriation');
+  }
+
+  tagAbbreviationsForBlin(): Promise<string[]> {
+    return this.tagAbbreviations('BLIN');
+  }
+
+  
 
 }
