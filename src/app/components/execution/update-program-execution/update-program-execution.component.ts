b77082bf2f5da51639bbb749d46f53efbf7a58b1
ry99 <ryan@ostrich-emulators.com>
2018-06-29 13:07:02 -0400

    added function stubs for transfering transfer to endpoint

--- src/app/components/execution/update-program-execution/update-program-execution.component.ts
+++ src/app/components/execution/update-program-execution/update-program-execution.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import * as $ from 'jquery';
+import { Component, OnInit, ViewChild } from '@angular/core'
+import * as $ from 'jquery'
 
 // Other Components
-import { HeaderComponent } from '../../../components/header/header.component';
+import { HeaderComponent } from '../../../components/header/header.component'
 import { PBService } from '../../../generated/api/pB.service'
 import { MyDetailsService } from '../../../generated/api/myDetails.service'
 import { ExecutionService } from '../../../generated/api/execution.service'
+import { ExecutionTransfer } from '../../../generated/model/executionTransfer'
 import { PB } from '../../../generated/model/pB'
-import { Execution } from '../../../generated/model/execution';
-import { Router } from '@angular/router';
+import { Execution } from '../../../generated/model/execution'
+import { Router } from '@angular/router'
 
 declare const $: any;
 declare const jQuery: any;
@@ -22,9 +23,23 @@ export class UpdateProgramExecutionComponent implements OnInit {
 
   @ViewChild(HeaderComponent) header;
 
-  constructor() { }
+  constructor(private exesvc:ExecutionService) { }
 
   ngOnInit() {
   }
 
+  submit() {
+    /*
+    var et: ExecutionTransfer = {
+      toIdAmtLkp: {},
+      fromId: 'from id',
+      eventType: 'etype',
+      transType: 'REALIGNMENT'
+    };
+    et.toIdAmtLkp['09848'] = 56;
+
+    this.exesvc.createTransfer("1234", new Blob(["stuff"]),
+      new Blob([JSON.stringify(et)])).subscribe();
+    */
+  }
 }
