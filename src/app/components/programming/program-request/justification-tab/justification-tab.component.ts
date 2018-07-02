47a628db7da21c9bd52bca8112199e985f277100
DanielCastro <castrov.daniel@gmail.com>
2018-07-02 10:41:53 -0400

    server/client: justification tab populating data and saving

--- src/app/components/programming/program-request/justification-tab/justification-tab.component.ts
+++ src/app/components/programming/program-request/justification-tab/justification-tab.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ProgrammaticRequest} from "../../../../generated";
 
 @Component({
   selector: 'justification-tab',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./justification-tab.component.scss']
 })
 export class JustificationTabComponent implements OnInit {
+
+  @Input()
+  pr : ProgrammaticRequest;
 
   constructor() { }
 
