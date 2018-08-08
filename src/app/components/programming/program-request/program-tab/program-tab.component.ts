337d9c0eb908db7693d7d1d24c2a65001ffb5191
Rossen <rossen.nedeltchev@p-exchange.com>
2018-08-08 19:12:26 -0400

    code for creating mismatching tags

--- src/app/components/programming/program-request/program-tab/program-tab.component.ts
+++ src/app/components/programming/program-request/program-tab/program-tab.component.ts
@@ -1,6 +1,6 @@
 import { GlobalsService } from './../../../../services/globals.service';
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
-import {FileResponse, LibraryService, ProgrammaticRequest, Tag} from "../../../../generated";
+import {Component, Input, OnChanges} from '@angular/core';
+import {FileResponse, LibraryService, ProgrammaticRequest, Tag} from '../../../../generated';
 import {DomSanitizer} from '@angular/platform-browser';
 
 
@@ -9,12 +9,12 @@ import {DomSanitizer} from '@angular/platform-browser';
   templateUrl: './program-tab.component.html',
   styleUrls: ['./program-tab.component.scss']
 })
-export class ProgramTabComponent implements OnInit, OnChanges {
-  @Input() pr : ProgrammaticRequest;
-  readonly fileArea= 'pr';
+export class ProgramTabComponent implements OnChanges {
+  @Input() pr: ProgrammaticRequest;
+  readonly fileArea = 'pr';
   imagePath: string;
 
-  readonly tagTypes: string[] = 
+  readonly tagTypes: string[] =
   ['Lead Component',
     'Manager',
     'Primary Capability',
@@ -23,24 +23,29 @@ export class ProgramTabComponent implements OnInit, OnChanges {
     'Functional Area',
     'Medical Category',
     'DASD CBD',
-    'Emphasis Areas'
-  ];
+    'Emphasis Areas'];
 
   readonly mapTypeToTags = new Map<string, Tag[]>();
+
+  *prFieldsGenerator() { // must match 'tagTypes: string[]'
+    yield this.pr.leadComponent;
+    yield this.pr.manager;
+    yield this.pr.primaryCapability;
+    yield this.pr.coreCapability;
+    yield this.pr.secondaryCapability;
+    yield this.pr.functionalArea;
+    yield this.pr.medicalArea;
+    yield null;  // this field is not fully implemented
+    return null; // this field is not fully implemented
+  }
 
   constructor(private globalsService: GlobalsService,
               private libraryService: LibraryService,
               private sanitization: DomSanitizer) {}
 
-  ngOnInit() {
-    this.tagTypes.forEach(tagType => 
-      this.globalsService.tags(tagType).subscribe((tags: Tag[]) => 
-        this.mapTypeToTags.set(tagType, tags)
-      )
-    );
-  }
-
   ngOnChanges() {
+    this.createMismatchingTags();
+
     if (this.pr.imageName) {
       this.libraryService.downloadFile(this.pr.imageName, this.fileArea).subscribe(response => {
         if (response.result) {
@@ -52,6 +57,28 @@ export class ProgramTabComponent implements OnInit, OnChanges {
     }
   }
 
+  private createMismatchingTags() {
+    const prFields: IterableIterator<string> = this.prFieldsGenerator();
+    this.tagTypes.forEach(async (tagType) => {
+      const prField: string = prFields.next().value;
+      const tags: Tag[] = await this.globalsService.tags(tagType).toPromise();
+      this.mapTypeToTags.set(tagType, tags);
+      this.createMismatchingTagIfNeeded(tags, tagType, prField);
+    });
+  }
+
+  private createMismatchingTagIfNeeded(tags: Tag[], tagType: string, prField: string) {
+    if (prField && this.tagAbbreviationIsNotFoundInTagAbbreviations(prField, tags)) {
+      const mismatchingTag = { abbr: prField, name: prField + ' (expanded name unavailable)' };
+      this.mapTypeToTags.get(tagType).push(mismatchingTag);
+    }
+  }
+
+  private tagAbbreviationIsNotFoundInTagAbbreviations(tagAbbreviation: string, tags: Tag[]) {
+    const tagAbbreviations = new Set(tags.map(tag => tag.abbr));
+    return !tagAbbreviations.has(tagAbbreviation);
+  }
+
   onFileUploaded(fileResponse: FileResponse){
     let imagePath = 'data:'+ fileResponse.contentType +';base64,'  + fileResponse.content;
     this.imagePath = this.sanitization.bypassSecurityTrustResourceUrl(imagePath) as string;
@@ -61,5 +88,5 @@ export class ProgramTabComponent implements OnInit, OnChanges {
 
   get invalid(): boolean {
     return !this.pr.leadComponent || !this.pr.manager || !this.pr.primaryCapability || !this.pr.coreCapability || !this.pr.functionalArea;
-  }  
+  }
 }
