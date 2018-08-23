52ca0e4ae88989d32c0997102102edc325299539
Rossen <rossen.nedeltchev@p-exchange.com>
2018-08-23 11:11:56 -0400

    refactorings

--- src/app/components/ufr/ufr-search/ufr-search.component.ts
+++ src/app/components/ufr/ufr-search/ufr-search.component.ts
@@ -1,22 +1,16 @@
-import { Component, OnInit, ViewChild, Input, AfterViewInit } from '@angular/core';
+import { RestResult } from './../../../generated/model/restResult';
+import { UserUtils } from './../../../services/user.utils.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatTableDataSource, MatPaginator, MatSort, MatSortable } from '@angular/material';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import { forkJoin } from "rxjs/observable/forkJoin";
-
 import { HeaderComponent } from '../../header/header.component';
-
 import { UFRsService } from '../../../generated/api/uFRs.service';
 import { UFR } from '../../../generated/model/uFR'
-import { MyDetailsService } from '../../../generated/api/myDetails.service';
 import { CommunityService } from '../../../generated/api/community.service';
 import { UFRFilter } from '../../../generated/model/uFRFilter';
-import {
-  OrganizationService, Organization, Community, POMService, PBService,
-  Pom, PB, ProgramsService, Tag, Program
-} from '../../../generated';
-
+import { OrganizationService, Organization, POMService, PBService, Pom, PB, ProgramsService, Tag, User, Program } from '../../../generated';
 import { ProgramTreeUtils } from '../../../utils/program-tree-utils'
-
 import { Cycle } from '../cycle';
 import { Disposition } from '../disposition.enum';
 import { Status } from '../status.enum';
@@ -31,152 +25,146 @@ export class UfrSearchComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sorter: MatSort;
 
-  private year: number = new Date().getFullYear() + 2;
-  private useorg: boolean = false;
-  private usefa: boolean = false;
-  private usedates: boolean = false;
-  private usestatus: boolean = false;
-  private usedisp: boolean = false;
-  private usecycle: boolean = false;
-  private filter: UFRFilter = {};
-  private orgs: Organization[] = [];
-  private fas: Tag[] = [];
-  private cycles: string[] = [];
-  private community: Community;
-  private dispositions: string[]=[];
+  private user: User;
+
+  private useOrganization: boolean = false;
+  private organizations: Organization[] = [];
+
+  private useFunctionalArea: boolean = false;
+  private functionalAreas: Tag[] = [];
+
+  private useDates: boolean = false;
+
+  private useStatus: boolean = false;
   private statuses: string[] = [];
+  
+  private useDisposition: boolean = false;
+  private dispositions: string[]=[];
+
+  private useCycle: boolean = false;
+  private cycles: string[] = [];
+
+  private ufrFilter: UFRFilter = {};
   private cyclelkp: Map<string, string> = new Map<string, string>();
 
   private datasource: MatTableDataSource<UFR> = new MatTableDataSource<UFR>();
-  private programlkp: Map<string, string> = new Map<string, string>();// mrid, fullname
+  private mapIdToName: Map<string, string> = new Map<string, string>();// mrid, fullname
   private editable: boolean = false;
 
-  constructor(private usvc: UFRsService, private userDetailsService: MyDetailsService,
-    private communityService: CommunityService, private orgsvc: OrganizationService,
-    private pomsvc: POMService, private pbsvc: PBService, private progsvc: ProgramsService,
-    private router: Router) {
-    
-    this.dispositions = Object.keys(Disposition)
-      .filter(k => typeof Disposition[k] === "number") as string[];
-    this.statuses = Object.keys(Status)
-      .filter(k => typeof Status[k] === "number") as string[];
+  constructor(private ufrsService: UFRsService, 
+              private userUtils: UserUtils,
+              private communityService: CommunityService, 
+              private organizationService: OrganizationService,
+              private pomService: POMService, 
+              private pbService: PBService,
+              private programsService: ProgramsService,
+              private router: Router) {
+    this.dispositions = Object.keys(Disposition).filter(k => typeof Disposition[k] === "number") as string[];
+    this.statuses = Object.keys(Status).filter(k => typeof Status[k] === "number") as string[];
   }
 
-  ngOnInit() {
-    var my: UfrSearchComponent = this;
+  async ngOnInit() {
+    this.user = await this.userUtils.user().toPromise();
 
-    this.userDetailsService.getCurrentUser().subscribe((person) => {
-      forkJoin([my.communityService.getById(person.result.currentCommunityId),
-        my.orgsvc.getByCommunityId(person.result.currentCommunityId),
-        my.pomsvc.getByCommunityId(person.result.currentCommunityId),
-        my.pbsvc.getByCommunityId(person.result.currentCommunityId),
-        my.progsvc.getTagsByType("Functional Area"),
-        my.progsvc.getAll()
-      ]).subscribe(data => {
-        my.community = data[0].result;
-        my.orgs = data[1].result;
-        my.fas = (data[4].result ? data[4].result : []);
-        
-        ProgramTreeUtils.fullnames(data[5].result).forEach((fullname, program) => {
-          my.programlkp.set(program.id, fullname);
-        });
+    const forkJoinResult: RestResult[] = await forkJoin([
+      this.organizationService.getByCommunityId(this.user.currentCommunityId), // 1
+      this.pomService.getByCommunityId(this.user.currentCommunityId), // 2
+      this.pbService.getByCommunityId(this.user.currentCommunityId), // 3
+      this.programsService.getTagsByType("Functional Area"), // 4
+      this.programsService.getAll() // 5
+    ]).toPromise();
+      
+    this.organizations = forkJoinResult[0].result;
+    this.functionalAreas = forkJoinResult[3].result || [];
 
-        // console.log( 'pomisopen: '+my.pomIsOpen)
+    this.initProgramLkp(forkJoinResult[4].result);
 
-        my.filter.orgId = my.orgs[0].id;
-        my.filter.from = new Date().getTime();
-        my.filter.to = new Date().getTime();
-        my.filter.status = Status[0];
-        my.filter.disposition = Disposition[0];
-        
-        my.fas.sort(function (a, b) { 
-          if (a.abbr === b.abbr) {
-            return 0;
-          }
-          return (a.abbr < b.abbr ? -1 : 1);
-        });
-        my.filter.fa = my.fas[0].abbr;
-
-        my.filter.yoe = true;
-        my.filter.active = true;
-
-        my.editable = false;
-        var phases: Cycle[] = [];
-        data[2].result.forEach(function (x: Pom) {
-          phases.push({
-            fy: x.fy,
-            phase: 'POM'
-          });
-          my.cyclelkp.set(x.id, 'POM ' + x.fy);
+    
+    this.functionalAreas.sort((tag1: Tag, tag2: Tag) => { 
+      if (tag1.abbr === tag2.abbr) {
+        return 0;
+      }
+      return (tag1.abbr < tag2.abbr ? -1 : 1);
+    });
+    
+    this.editable = false; // this.initCycles() changes it
+    this.initCycles(forkJoinResult[1].result, forkJoinResult[2].result);
+    
+    this.initUfrFilter();
+    
+    this.search();
+  }
 
 
-          if ('CREATED' === x.status || 'OPEN' === x.status) {
-            my.editable = true;
-          }
-        });
-        data[3].result.forEach(function (x: PB) {
-          phases.push({
-            fy: x.fy,
-            phase: 'PB'
-          });
-        });
+  private initUfrFilter() {
+    this.ufrFilter.orgId = this.organizations[0].id;
+    this.ufrFilter.from = new Date().getTime();
+    this.ufrFilter.to = new Date().getTime();
+    this.ufrFilter.status = Status[0];
+    this.ufrFilter.disposition = Disposition[0];
+    this.ufrFilter.cycle = this.cycles[0];
+    this.ufrFilter.fa = this.functionalAreas[0].abbr;
+    this.ufrFilter.yoe = true;
+    this.ufrFilter.active = true;
+  }
 
-        phases.sort(function (a:any, b:any) {
-          if (a.fy === b.fy) {
-            if (a.phase === b.phase) {
-              return 0;
-            }
-            return (a.phase < b.phase ? -1 : 1);
-          }
-          else {
-            return a.fy - b.fy;
-          }
-        });
+  private initCycles(poms: Pom[], pbs: PB[]) {
+    const phases: Cycle[] = [];
 
-        phases.forEach(function (x:any) { 
-          my.cycles.push( x.phase + ' ' + (x.fy - 2000));
-        });
-        my.filter.cycle = my.cycles[0];
-        my.search();
-      });
+    poms.forEach((pom: Pom) => {
+      phases.push({ fy: pom.fy, phase: 'POM' });
+      this.cyclelkp.set(pom.id, 'POM ' + pom.fy);
+      if ('CREATED' === pom.status || 'OPEN' === pom.status) {
+        this.editable = true;
+      }
+    });
+
+    pbs.forEach((pb: PB) => {
+      phases.push({ fy: pb.fy, phase: 'PB' });
+    });
+
+    phases.sort((cycle1: Cycle, cycle2: Cycle) => {
+      if (cycle1.fy === cycle2.fy) {
+        if (cycle1.phase === cycle2.phase) {
+          return 0;
+        }
+        return (cycle1.phase < cycle2.phase ? -1 : 1);
+      }
+      else {
+        return cycle1.fy - cycle2.fy;
+      }
+    });
+    phases.forEach((cycle: Cycle) => {
+      this.cycles.push(cycle.phase + ' ' + (cycle.fy - 2000));
+    });
+  }
+
+  private initProgramLkp(programs: Program[]) {
+    ProgramTreeUtils.fullnames(programs).forEach((fullname, program) => {
+      this.mapIdToName.set(program.id, fullname);
     });
   }
 
   search() {
-    var my: UfrSearchComponent = this;
     var searchfilter: UFRFilter = {};
-    if (my.usecycle) {
-      searchfilter.cycle = my.filter.cycle.replace(/([0-9]+)/, "20$1");
+    if (this.useCycle) searchfilter.cycle = this.ufrFilter.cycle.replace(/([0-9]+)/, "20$1");
+    if (this.useDates) {
+      searchfilter.from = this.ufrFilter.from;
+      searchfilter.to = this.ufrFilter.to;
     }
-    if (my.usedates) {
-      searchfilter.from = my.filter.from;
-      searchfilter.to = my.filter.to;
-    }
-    if (my.usedisp) {
-      searchfilter.disposition = my.filter.disposition.toUpperCase().replace( ' ', '_' );
-    }
-    if (my.usefa) {
-      searchfilter.fa = my.filter.fa;
-    }
-    if (my.useorg) {
-      searchfilter.orgId = my.filter.orgId;
-    }
-    if (my.usestatus) {
-      searchfilter.status = my.filter.status;
-    }
+    if (this.useDisposition) searchfilter.disposition = this.ufrFilter.disposition.toUpperCase().replace( ' ', '_' );
+    if (this.useFunctionalArea) searchfilter.fa = this.ufrFilter.fa;
+    if (this.useOrganization) searchfilter.orgId = this.ufrFilter.orgId;
+    if (this.useStatus) searchfilter.status = this.ufrFilter.status;
 
-    //console.log(my.community.id);
-    //console.log(searchfilter);
-    this.usvc.search( my.community.id, searchfilter ).subscribe(
+    this.ufrsService.search( this.user.currentCommunityId, searchfilter ).subscribe(
       (data) => {
-        my.datasource.data = data.result;
-        //console.log(data.result);
-        //my.datasource.data = my.makeMockUfrs();
+        this.datasource.data = data.result;
         // FIXME: I think these lines belong in ngAfterViewInit, but I can't get
         // it to work there. The sorter and paginator aren't set there (?)
         // so this is a not-too-ugly workaround.
-        my.datasource.sort = my.sorter;
-        my.datasource.paginator = my.paginator;
+        this.datasource.sort = this.sorter;
+        this.datasource.paginator = this.paginator;
       });
   }
 
@@ -184,25 +172,24 @@ export class UfrSearchComponent implements OnInit {
     this.router.navigate(['/ufr-view', row.id]);
   }
 
-  getProgId(p: UFR) {
-    return this.programlkp.get( p.originalMrId );
+  getProgramName(ufr: UFR): string {
+    return this.mapIdToName.get( ufr.originalMrId );
   }
 
-  getParentProgId(p: UFR) {
-    return (p.parentMrId ? this.programlkp.get(p.parentMrId) : '');
+  getParentProgramName(ufr: UFR): string {
+    return (ufr.parentMrId ? this.mapIdToName.get(ufr.parentMrId) : '');
   }
 
-  getFullProgId(p: UFR) {
-    var parentname = this.getParentProgId(p);
-    if ('' != parentname) {
-      parentname += '/';
-    }
+  // Only considers the immediate parent, i.e. cannot consider three levels in the hierarchy, i.e. AAA/BBB/CCC
+  getFullProgramName(ufr: UFR): string {
+    var parentName = this.getParentProgramName(ufr);
+    if (parentName) parentName += '/';
 
     // this UFR might be a new program, and if so, just use the shortname
-    if (p.originalMrId) {
-      return parentname + this.getProgId(p);
+    if (ufr.originalMrId) {
+      return parentName + this.getProgramName(ufr);
     }
 
-    return parentname + (!p.shortName || null === p.shortName ? '??' : p.shortName);
+    return parentName + (!ufr.shortName || null === ufr.shortName ? '??' : ufr.shortName);
   }
 }
