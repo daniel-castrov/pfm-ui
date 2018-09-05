f0a867616ea9ef3cdb1ec6e98aec6d95a971d8c0
Rossen <rossen.nedeltchev@p-exchange.com>
2018-09-05 18:57:53 -0400

    displayed ufrNumber and POM fiscal year

--- src/app/components/ufr/ufr-view/ufr-view.component.ts
+++ src/app/components/ufr/ufr-view/ufr-view.component.ts
@@ -1,8 +1,8 @@
-import { CycleUtils } from './../../../services/cycle.utils';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { UFR, UFRsService } from '../../../generated';
-import { HeaderComponent } from '../../header/header.component';
-import { ActivatedRoute, UrlSegment } from '@angular/router';
+import {CycleUtils} from './../../../services/cycle.utils';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {POMService, UFR, UFRsService} from '../../../generated';
+import {HeaderComponent} from '../../header/header.component';
+import {ActivatedRoute, UrlSegment} from '@angular/router';
 
 @Component({
   selector: 'app-ufr-view',
@@ -13,21 +13,24 @@ export class UfrViewComponent implements OnInit {
   @ViewChild(HeaderComponent) header;
   private ufr: UFR;
   private canedit: boolean = false;
+  private pomFy: number;
 
   constructor( private ufrService: UFRsService,
                private cycleUtils: CycleUtils,
-               private route: ActivatedRoute ) {}
+               private route: ActivatedRoute,
+               private pomService: POMService) {}
 
   async ngOnInit() {
     this.route.url.subscribe(async(urlSegments: UrlSegment[]) => { // don't try to convert this one to Promise -- it doesn't work
       const ufrId = urlSegments[urlSegments.length - 1].path;
-      this.initUfr(ufrId);
+      this.init(ufrId);
       this.canedit = !!await this.cycleUtils.currentPom().toPromise();
     });
   }
 
-  private async initUfr(ufrId: string) {
+  private async init(ufrId: string) {
     this.ufr = (await this.ufrService.getUfrById(ufrId).toPromise()).result;
+    this.pomFy = (await this.pomService.getById(this.ufr.phaseId).toPromise()).result.fy  - 2000;
   }
 
   save() {
@@ -37,5 +40,10 @@ export class UfrViewComponent implements OnInit {
   submit() {
     this.ufr.status = 'SUBMITTED';
     this.save();
+  }
+
+  get ufrNumber(): string {
+    const sequentialNumber = ('000' + this.ufr.requestNumber).slice(-3);
+    return this.pomFy + sequentialNumber;
   }
 }
