065c321009596d89bae7d957cf921bd83c0b9a00
Rossen <rossen.nedeltchev@p-exchange.com>
2018-09-12 06:13:24 -0400

    Revert "Revert "Filtered out the OUTSTANDING pr from the selectable PRs for creating a UFR""
    This reverts commit 321fbf67263f56c27cf9d78f6c1780bf039ef40b.

--- src/app/components/ufr/ufr-search/new-ufr/new-ufr.component.ts
+++ src/app/components/ufr/ufr-search/new-ufr/new-ufr.component.ts
@@ -5,11 +5,11 @@ import { Router } from '@angular/router';
 import {UFRsService, Program, UFR, ShortyType, FundingLine, ProgramsService, PRService} from '../../../../generated';
 
 enum CreateNewUfrMode {
-  AN_MRDB_PROGRAM        = 'An MRDB Program',
+  AN_MRDB_PROGRAM = 'An MRDB Program',
   A_PROGRAMMATIC_REQUEST = 'A Programmatic Request',
-  A_NEW_INCREMENT        = 'A New Increment',
-  A_NEW_FOS              = 'A New FoS',
-  A_NEW_PROGRAM          = 'A New Program'
+  A_NEW_INCREMENT = 'A New Increment',
+  A_NEW_FOS = 'A New FoS',
+  A_NEW_PROGRAM = 'A New Program'
 }
 
 @Component({
@@ -40,25 +40,42 @@ export class NewUfrComponent implements OnInit {
 
   async setCreateNewUfrMode(createNewUfrMode: CreateNewUfrMode) {
     this.createNewUfrMode = createNewUfrMode;
-    switch(this.createNewUfrMode) {
+    switch (this.createNewUfrMode) {
       case 'An MRDB Program':
         this.selectableProgramsOrPrs = await this.withFullNameService.programsMunisPrs(this.allPrograms, this.pomId);
         this.initialSelectOption = 'Program';
         break;
       case 'A Programmatic Request': // was subprogram
-        this.selectableProgramsOrPrs = await this.withFullNameService.programRequestsWithFullNamesDerivedFromCreationTimeData(this.pomId);
+        let prs:any = await this.withFullNameService.programRequestsWithFullNamesDerivedFromCreationTimeData(this.pomId);
+        this.selectableProgramsOrPrs = this.removeOnlyPrsInOutandingState(prs);
         this.initialSelectOption = 'Program Request';
         break;
       case 'A New FoS':
       case 'A New Increment':
-        this.selectableProgramsOrPrs = await this.withFullNameService.programPlusPrsMinusPrsForGenericSubprograms(this.pomId);
+        let progsOrPrs:any = await this.withFullNameService.programPlusPrsMinusPrsForGenericSubprograms(this.pomId);
+        this.selectableProgramsOrPrs = this.removeOnlyPrsInOutandingState(progsOrPrs);
         this.initialSelectOption = 'Program';
         break;
     }
   }
 
-  generateEmptyFundingLine(fundingLines: FundingLine[]): FundingLine[]{
-    if(fundingLines) {
+  removeOnlyPrsInOutandingState(progsOrPrs?: any[]) {
+    let newPRs:any[] = [];
+    progsOrPrs.forEach((item, index) => {
+      // programs don't have a state 
+      if ( !item.state ) {
+        newPRs.push(item);
+      }
+      // prs in OUTSTNADING are not valid for creating a UFR 
+      else if ( item.state != "OUTSTANDING" ) {
+        newPRs.push(item);
+      }
+    });
+    return newPRs;
+  }
+
+  generateEmptyFundingLine(fundingLines: FundingLine[]): FundingLine[] {
+    if (fundingLines) {
       let emptyFundingLines = JSON.parse(JSON.stringify(fundingLines));
       emptyFundingLines.forEach(fl => {
         Object.keys(fl.funds).forEach(year => {
@@ -70,11 +87,11 @@ export class NewUfrComponent implements OnInit {
   }
 
   async next() {
-    let ufr: UFR = {phaseId: this.pomId};
+    let ufr: UFR = { phaseId: this.pomId };
     if (this.selectedProgramOrPr) {
       ufr.fundingLines = this.generateEmptyFundingLine(this.selectedProgramOrPr.fundingLines);
     }
-    switch(this.createNewUfrMode) {
+    switch (this.createNewUfrMode) {
       case 'An MRDB Program':
         ufr.shortyType = ShortyType.MRDB_PROGRAM;
         ufr.shortyId = this.selectedProgramOrPr.id;
@@ -86,7 +103,7 @@ export class NewUfrComponent implements OnInit {
         this.initFromShortyPR(ufr);
         break;
       case 'A New FoS':
-        if(this.withFullNameService.isProgram(this.selectedProgramOrPr)) {
+        if (this.withFullNameService.isProgram(this.selectedProgramOrPr)) {
           ufr.shortyType = ShortyType.NEW_FOS_FOR_MRDB_PROGRAM;
           ufr.shortyId = this.selectedProgramOrPr.id;
           this.initFromShortyProgram(ufr);
@@ -97,7 +114,7 @@ export class NewUfrComponent implements OnInit {
         }
         break;
       case 'A New Increment':
-        if(this.withFullNameService.isProgram(this.selectedProgramOrPr)) {
+        if (this.withFullNameService.isProgram(this.selectedProgramOrPr)) {
           ufr.shortyType = ShortyType.NEW_INCREMENT_FOR_MRDB_PROGRAM;
           ufr.shortyId = this.selectedProgramOrPr.id;
           this.initFromShortyProgram(ufr);
@@ -109,7 +126,7 @@ export class NewUfrComponent implements OnInit {
         break;
       case 'A New Program':
         ufr.shortyType = ShortyType.NEW_PROGRAM;
-      break;
+        break;
     }
     ufr = (await this.ufrService.create(ufr).toPromise()).result;
     this.router.navigate(['/ufr-view', ufr.id]);
