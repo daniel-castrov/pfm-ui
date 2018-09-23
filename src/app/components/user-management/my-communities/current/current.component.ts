e59ec3a0a91dc11958ad1d4037d9f935a6bb40fa
Michael Giordano <michael.giordano@p-exchange.com>
2018-09-23 16:35:58 -0400

    Completed adding the organization to the My Communities page.

--- src/app/components/user-management/my-communities/current/current.component.ts
+++ src/app/components/user-management/my-communities/current/current.component.ts
@@ -5,6 +5,9 @@ import { HeaderComponent } from '../../../header/header.component';
 import { Community } from '../../../../generated';
 import { User } from '../../../../generated';
 import { MyDetailsService } from '../../../../generated/api/myDetails.service';
+import { OrganizationService } from '../../../../generated/api/organization.service';
+import { Organization } from '../../../../generated';
+
 
 @Component({
   selector: 'j-current',
@@ -15,29 +18,57 @@ export class CurrentComponent {
 
   @Input() header: HeaderComponent;
   @Input() private memberOfCommunities: Community[];
-  @Input() private user: User;
+  private _user: User;
   private selectedCommunityId: string;
   private communitiesToSelectFrom: Community[];
+  private currentOrg: Organization;
 
-  constructor(private myDetailsService: MyDetailsService) {
-
+  constructor(
+    private myDetailsService: MyDetailsService, 
+    private orgService: OrganizationService) {
   }
 
   ngOnChanges() {
       this.updateCommunitiesToSelectFrom();
   }
 
+
+  @Input() set user( u:User ){
+    this._user = u;
+    if (u) {
+      this.orgService.get(u.organizationId).subscribe( org => {
+        this.currentOrg = org.result;
+      });
+    }
+  }
+
+  getCurrentOrg(u:User){
+    this.orgService.get(u.organizationId).subscribe( org => {
+      this.currentOrg = org.result;
+    });
+  }
+
   private change() {
-    const user: User = {...this.user, currentCommunityId : this.selectedCommunityId};
-    this.myDetailsService.updateCurrentUser(user).subscribe(() => {
-      this.user.currentCommunityId = this.selectedCommunityId;
-      this.updateCommunitiesToSelectFrom();
-      this.header.refreshActions();
-      delete this.selectedCommunityId;
+    const user: User = {...this._user, currentCommunityId : this.selectedCommunityId};
+    this.myDetailsService.updateCurrentUser(user).subscribe( data => {
+      this.myDetailsService.getCurrentUser().subscribe ( data2 => {
+        this._user = data2.result;
+        this.updateCommunitiesToSelectFrom();
+        this.header.refreshActions();
+        delete this.selectedCommunityId;
+        this.orgService.get(this._user.organizationId).subscribe( org => {
+          this.currentOrg = org.result;
+        });
+      });
+
+
+      this._user.currentCommunityId = this.selectedCommunityId;
+      
+      
     });
   }
 
   private updateCommunitiesToSelectFrom(): void {
-    this.communitiesToSelectFrom = this.memberOfCommunities.filter(community => community.id != this.user.currentCommunityId);
+    this.communitiesToSelectFrom = this.memberOfCommunities.filter(community => community.id != this._user.currentCommunityId);
   }
 }
