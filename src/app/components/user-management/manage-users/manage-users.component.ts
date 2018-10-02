3cd43b6f3a0234880becf31997785fa9049c7805
Michael Giordano <michael.giordano@p-exchange.com>
2018-10-02 15:53:40 -0400

    Admin can now change a users Organization

--- src/app/components/user-management/manage-users/manage-users.component.ts
+++ src/app/components/user-management/manage-users/manage-users.component.ts
@@ -58,7 +58,7 @@ export class ManageUsersComponent {
   private isdEditMode: boolean[] = [];
 
   // Any error from a rest service
-  private resultError: string[]=[];
+  private resultError: string[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -89,28 +89,40 @@ export class ManageUsersComponent {
       this.resultError.push(data[0].error);
       this.targetUser = data[0].result;
 
-      let allCommunities: Community[] = [];
-
       this.resultError.push(data[1].error);
-      allCommunities = data[1].result;
-
+      let allCommunities = data[1].result;
+      
       for (let comm of allCommunities) {
-        let resultUserRoleResources: RestResult;
-        let s = this.roleService.getByUserIdAndCommunityId(this.targetUser.id, comm.id);
-        s.subscribe(c => {
-          resultUserRoleResources = c;
-          this.resultError.push(resultUserRoleResources.error);
 
-          // push the community if at least one role and take it out of avail
-          if ( resultUserRoleResources.result.length>0 ){
-            this.communityWithUserRoles.push(new CommWithRoles(comm, resultUserRoleResources.result));
-            for (var i =0; i < allCommunities.length; i++){
-              if ( allCommunities[i].id === comm.id ){
-                allCommunities.splice(i,1);
-              }
-            }
+        Observable.forkJoin([
+        this.roleService.getByUserIdAndCommunityId(this.targetUser.id, comm.id),
+        this.organizationService.getByCommunityId(comm.id)
+        ]).subscribe(data => {
+          
+          this.resultError.push(data[0].error);
+          let roles = data[0].result;
+
+          this.resultError.push(data[1].error);
+          let allOrganizations = data[1].result;
+
+          if (roles.length > 0) {
+
+            this.userRoleResourceService.getUserRoleByUserAndCommunityAndRoleName(
+              this.targetUser.id, comm.id, "Organization_Member")
+              .subscribe( data =>{
+                let commOrgId = data.result.resourceIds[0];
+                let commOrgs = allOrganizations.filter(org => org.communityId == comm.id);
+                this.communityWithUserRoles.push(
+                  new CommWithRoles(comm, roles, commOrgs, commOrgs.find( org => org.id == commOrgId))
+                );
+                for (var i = 0; i < allCommunities.length; i++) {
+                  if (allCommunities[i].id === comm.id) {
+                    allCommunities.splice(i, 1);
+                  }
+                }
+            });
           }
-          this.availCommunities=allCommunities;
+          this.availCommunities = allCommunities;
         });
       }
       this.reftargetUser = JSON.parse(JSON.stringify(this.targetUser));
@@ -128,8 +140,8 @@ export class ManageUsersComponent {
 
   // set a section to edit mode
   private editMode(sectionnumber): void {
-    this.selectedJoinCommunity=null;
-    this.selectedOrganization=null;
+    this.selectedJoinCommunity = null;
+    this.selectedOrganization = null;
     this.resetEditMode();
     this.isdEditMode[sectionnumber] = true;
   }
@@ -147,39 +159,58 @@ export class ManageUsersComponent {
   }
 
   private joinCommunity(): void {
-   
+
+
+    console.log(this.selectedOrganization);
+
     this.userRoleResourceService.joinCommunity(this.targetUser.id, this.selectedOrganization)
-      .subscribe( data => {
+      .subscribe(data => {
         this.refresh();
-      });
+    });
   }
 
-  private leaveCommunity(cwr:CommWithRoles): void {
+  private leaveCommunity(cwr: CommWithRoles): void {
 
-    let userRole:Role = cwr.userRoles.find( role => role.name == "User" );
+    let userRole: Role = cwr.userRoles.find(role => role.name == "User");
     this.userRoleResourceService.getUserRoleByUserAndCommunityAndRoleName(
-      this.targetUser.id, userRole.communityId, "User"
-    ).subscribe( data => {
-      let urr:UserRoleResource = data.result;  
-      this.userRoleResourceService.deleteById(urr.id).subscribe( data=> {
-      this.refresh();
+      this.targetUser.id, cwr.community.id, "User"
+    ).subscribe(data => {
+      let urr: UserRoleResource = data.result;
+      this.userRoleResourceService.deleteById(urr.id).subscribe(data => {
+        this.refresh();
       });
     });
   }
 
-  private editRoles( commid, roleid, userid ){
+  private changeOrg(cwr: CommWithRoles): void {
+
+    if (cwr.org.id != this.targetUser.organizationId) {
+      
+      this.userRoleResourceService.getUserRoleByUserAndCommunityAndRoleName(
+        this.targetUser.id, cwr.community.id, "Organization_Member")
+        .subscribe(data => {
+          let urr: UserRoleResource = data.result;
+          urr.resourceIds = [cwr.org.id];
+          this.userRoleResourceService.update(urr).subscribe(data => {
+            this.refresh();
+          })
+        });
+    }
+  }
+
+  private editRoles(commid, roleid, userid) {
     this.router.navigate(['/roles', commid, roleid, userid]);
   }
 
   private getOrganizations(): void {
     this.organizationService.getByCommunityId(this.selectedJoinCommunity.id)
-    .subscribe( data => this.organizations= data.result );
+      .subscribe(data => this.organizations = data.result);
   }
 
-  private refresh(){
-    this.communityWithUserRoles=[];
-    this.selectedJoinCommunity=null;
-    this.selectedOrganization=null;
+  private refresh() {
+    this.communityWithUserRoles = [];
+    this.selectedJoinCommunity = null;
+    this.selectedOrganization = null;
     this.initTargetUserData();
   }
 
@@ -187,11 +218,15 @@ export class ManageUsersComponent {
 
 class CommWithRoles {
 
-  community: Community;
-  userRoles: Role[] = [];
+  community: Community;       // The Community of interest
+  userRoles: Role[] = [];     // The tagerUser's roles in this Community
+  orgs: Organization[] = [];  // All the Organizations in thls Community
+  org: Organization;          // The targetUer's Organization in this Community
 
-  constructor(c: Community, ur: Role[]) {
+  constructor(c: Community, urs: Role[], ogs: Organization[], og: Organization) {
     this.community = c;
-    this.userRoles = ur;
+    this.userRoles = urs.filter(role => role.name != "User" && role.name != "Organization_Member");
+    this.orgs = ogs;
+    this.org = og;
   }
 }
