3ecd5bf66ea7d4515a9d4e69355f19b14f06c488
Michael Giordano <michael.giordano@p-exchange.com>
2018-10-10 09:36:31 -0400

    Changed the values for the services dropdown in the variants components

--- src/app/components/programming/program-request/variants-tab/variants-tab.component.ts
+++ src/app/components/programming/program-request/variants-tab/variants-tab.component.ts
@@ -27,7 +27,7 @@ export class VariantsTabComponent {
   showAddVariant=false;
   newVariantName:string;
 
-  branches:string[]=["USA","USN","USAF","USMC","USCG","USCG"];
+  branches:string[]=["USA","USN","USAF","USMC","SOCOM","NGB"];
 
   years:number[];
   columnDefs;
