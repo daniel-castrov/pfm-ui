8485fbc65db65f0d9f36a899a25bb1a5880b8a1e
Michael Giordano <michael.giordano@p-exchange.com>
2018-10-31 14:37:38 -0400

    just a stash really

--- src/app/components/programming/program-request/program-tab/program-tab.component.ts
+++ src/app/components/programming/program-request/program-tab/program-tab.component.ts
@@ -70,6 +70,7 @@ export class ProgramTabComponent implements OnChanges {
   private createMismatchingTagIfNeeded(tags: Tag[], tagType: string, prField: string) {
     if (prField && this.tagAbbreviationIsNotFoundInTagAbbreviations(prField, tags)) {
       const mismatchingTag = { abbr: prField, name: prField + ' (expanded name unavailable)' };
+      console.log( mismatchingTag );
       this.mapTypeToTags.get(tagType).push(mismatchingTag);
     }
   }
