ce7da09f9f9e9cf12368ba8f9d0d27e89c7ee56d
ry99 <ryan@ostrich-emulators.com>
2018-11-06 15:40:31 -0500

    added open endpoint. still need some logic work

--- src/app/components/execution/open-execution/open-execution.component.ts
+++ src/app/components/execution/open-execution/open-execution.component.ts
@@ -8,6 +8,7 @@ import { GridOptions } from 'ag-grid';
 import { forkJoin } from 'rxjs/observable/forkJoin';
 import { SpendPlanService } from '../../../generated';
 import { SpendPlan } from '../../../generated';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-open-execution',
@@ -21,9 +22,9 @@ export class OpenExecutionComponent implements OnInit {
   private agOptions: GridOptions;
   private exelines: ExecutionLine[];
   private spendplans: Map<string, SpendPlan> = new Map<string, SpendPlan>();
-
-  constructor(private exesvc: ExecutionService, private spsvc:SpendPlanService,
-    private userutils: UserUtils) { 
+  
+  constructor(private exesvc: ExecutionService, private spsvc: SpendPlanService,
+    private userutils: UserUtils) {
     
     var my: OpenExecutionComponent = this;
 
@@ -78,7 +79,7 @@ export class OpenExecutionComponent implements OnInit {
           cellClass: ['ag-cell-light-grey', 'ag-clickable'],
           valueGetter: p => (my.spendplans.has(p.data.id)
             ? 'Complete'
-            : 'Missing' ),
+            : 'Missing'),
         },
       ]
     };
@@ -86,7 +87,7 @@ export class OpenExecutionComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.userutils.user().subscribe(user => { 
+    this.userutils.user().subscribe(user => {
       this.exesvc.getByCommunityId(user.currentCommunityId, Execution.TypeEnum.CREATED).subscribe(exes => {
         this.phases = exes.result;
         if (this.phases.length > 0) {
@@ -101,9 +102,9 @@ export class OpenExecutionComponent implements OnInit {
     forkJoin([
       this.exesvc.getExecutionLinesByPhase(this.phase.id),
       this.spsvc.getByExecutionPhaseId(this.phase.id, SpendPlan.TypeEnum.BASELINE)
-    ]).subscribe((d:any) => {
+    ]).subscribe((d: any) => {
       var ok: boolean = true;
-      d.forEach( (xx:any) => { 
+      d.forEach((xx: any) => {
         if (xx.error) {
           Notify.error(xx.error);
           ok = false;
@@ -112,10 +113,34 @@ export class OpenExecutionComponent implements OnInit {
 
       if (ok) {
         this.spendplans.clear();
-        d[1].result.forEach( (sp:SpendPlan) => { 
+        d[1].result.forEach((sp: SpendPlan) => {
           this.spendplans.set(sp.executionLineId, sp);
         });
         this.exelines = d[0].result;
+      }
+    });
+  }
+
+  submitable() {
+    return (this.exelines && this.exelines.length === this.spendplans.size);
+  }
+
+  openExe() {
+    this.exesvc.openExecution(this.phase.id).subscribe(d => { 
+      console.log(d);
+    });
+  }
+
+  demoCreatePlans() {
+    // create spend plans for everybody without a spend plan, then open the phase
+    this.exelines.forEach(el => { 
+      if (!this.spendplans.has(el.id)) {
+        this.spsvc.createSpendPlan(el.id, {
+          type: SpendPlan.TypeEnum.BASELINE
+        }).subscribe(r => {
+          this.spendplans.set(el.id, r.result);
+          this.agOptions.api.refreshCells();
+        });
       }
     });
   }
