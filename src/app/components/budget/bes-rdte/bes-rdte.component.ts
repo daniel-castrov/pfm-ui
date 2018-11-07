118126ef3533b0389622c5ecb32c68b5f00ecd83
Rossen <rossen.nedeltchev@p-exchange.com>
2018-11-07 14:29:52 -0500

    code cleanup

--- src/app/components/budget/bes-rdte/bes-rdte.component.ts
+++ src/app/components/budget/bes-rdte/bes-rdte.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { AgGridNg2 } from 'ag-grid-angular';
-import { HeaderComponent } from '../../header/header.component';
+import {Component, ViewChild} from '@angular/core';
+import {AgGridNg2} from 'ag-grid-angular';
+import {HeaderComponent} from '../../header/header.component';
 
 @Component({ 
   selector: 'app-bes-rdte',
@@ -15,43 +15,21 @@ export class BesRdteComponent {
   private gridApi;
   private gridColumnApi;
   private columnDefs;
-  private rowData;
-  private groupDefaultExpanded;
-  private getTreeHierarchy;
-  private autoGroupColumnDef;
+  private rowData = [];
+  private groupDefaultExpanded = -1;
+  private autoGroupColumnDef = {
+    headerName: "",
+    maxWidth: 400,
+    cellRendererParams: { suppressCount: true }
+  };
 
-  items: any[] = [];
+  items = [];
 
   constructor() {
-
-    this.columnDefs = [
-      {
-        field: "form",
-        headerName: "Form",
-        maxWidth: 120,
-      },
-      {
-        field: "responsible",
-        headerName: "Responsible",
-        maxWidth: 120,
-      },
-      {
-        field: "desc",
-        headerName: "Description",
-      },
-    ];
-
+    this.columnDefs = [{field: "form",headerName: "Form",maxWidth: 120},
+                       {field: "responsible",headerName: "Responsible",maxWidth: 120},
+                       {field: "desc",headerName: "Description"}];
     this.initRowData();
-
-    this.groupDefaultExpanded = -1;
-
-    this.getTreeHierarchy = this.treeHierarchy;
-
-    this.autoGroupColumnDef = {
-      headerName: "",
-      maxWidth: 400,
-      cellRendererParams: { suppressCount: true }
-    };
   }
 
   treeHierarchy(data) {
@@ -65,256 +43,99 @@ export class BesRdteComponent {
   onGridReady(params) {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
-
     params.api.sizeColumnsToFit();
   }
 
   initRowData() {
-
-    this.getDemoData();
-
-    let data: any[] = [];
-
-    this.getOverviewAndR1( ["RDTE"] ).forEach( x => data.push(x) );
+    this.items = this.getItems();
+    this.getOverviewAndR1( ["RDTE"] ).forEach( x => this.rowData.push(x) );
 
     for ( let i=1; i<8; i++ ){
-
-      let ba:string = "BA-"+i;
-
-      
-
+      const ba = "BA-" + i;
       let itempe="";
-      this.items.filter( itm =>  itm.ba==i.toString()  ).forEach( item => {
+
+      const itemsWithCurrentBa = this.items.filter(item => item.ba == i);
+
+      itemsWithCurrentBa.forEach(item => {
 
         if ( item.pe != itempe ){
           itempe = item.pe;
-          this.getR2Fields(["RDTE", ba, "PE:" + item.pe, "R-2"], item).forEach( x => data.push(x) );
+          this.getR2Fields(["RDTE", ba, "PE:" + item.pe, "R-2"], item).forEach( x => this.rowData.push(x) );
         }
         
-        this.getR2AFields( ["RDTE", ba, "PE:"+item.pe, "ITEM: "+item.inum, "R-2A"], item ).forEach( x => data.push(x) );
+        this.getR2AFields( ["RDTE", ba, "PE:"+item.pe, "ITEM: "+item.inum, "R-2A"], item ).forEach( x => this.rowData.push(x) );
 
-        if  ( item.fyt > 10 && (item.ba == "4" || item.ba == "5" || item.ba == "7") ){
-          this.getR3Fields(  ["RDTE", ba, "PE:"+item.pe, "ITEM: "+item.inum,"R-3"],  item).forEach( x => data.push(x) );
-          this.getR4Fields(  ["RDTE", ba, "PE:"+item.pe, "ITEM: "+item.inum,"R-4"],  item).forEach( x => data.push(x) );
-          this.getR4AFields( ["RDTE", ba, "PE:"+item.pe, "ITEM: "+item.inum,"R-4A"], item).forEach( x => data.push(x) );
+        if ( item.fyt > 10 && (item.ba == "4" || item.ba == "5" || item.ba == "7") ){
+          this.getR3Fields(  ["RDTE", ba, "PE:"+item.pe, "ITEM: "+item.inum,"R-3"],  item).forEach( x => this.rowData.push(x) );
+          this.getR4Fields(  ["RDTE", ba, "PE:"+item.pe, "ITEM: "+item.inum,"R-4"],  item).forEach( x => this.rowData.push(x) );
+          this.getR4AFields( ["RDTE", ba, "PE:"+item.pe, "ITEM: "+item.inum,"R-4A"], item).forEach( x => this.rowData.push(x) );
         }
       });
     }
-    this.rowData = data;
   }
 
-  getOverviewAndR1(hier) :any {
-
-    let data:any[] = [];
-
-    data.push({
-      hierarchy: hier.concat( "FY" ),
-      responsible: "Budget Manager",
-      form: "Title",
-      desc: "THe Budget Year"
-    });
-
-    data.push({
-      hierarchy: hier.concat( "Cycle" ),
-      responsible: "Budget Manager",
-      form: "Title",
-      desc: "BES or PB"
-    });
-
-    data.push({
-      hierarchy: hier.concat( "Title" ),
-      responsible: "Budget Manager",
-      form: "Title",
-      desc: "A Title"
-    });
-
-    data.push({
-      hierarchy: hier.concat( "Community" ),
-      responsible: "Budget Manager",
-      form: "Title",
-      desc: "CBDP"
-    });
-
-    data.push({
-      hierarchy: hier.concat( "Type" ),
-      responsible: "Budget Manager",
-      form: "Title",
-      desc: "Type of JB"
-    });
-
-    data.push({
-      hierarchy: hier.concat( "Appropriation" ),
-      responsible: "Budget Manager",
-      form: "Title",
-      desc: "RDT&E or PROC"
-    });
-
-    data.push({
-      hierarchy: hier.concat( "Overview" ),
-      responsible: "Budget Manager",
-      form: "Overview",
-      desc: "A file with an overall desription"
-    });
-
-    data.push({
-      hierarchy: hier.concat( "R-1" ),
-      responsible: "Budget Manager",
-      form: "R-1",
-      desc: "A file that is the R-1 form"
-    });
+  getOverviewAndR1(hierarchy: string[]) {
+    let data = [];
+    data.push({ hierarchy: hierarchy.concat( "FY" ), responsible: "Budget Manager", form: "Title", desc: "THe Budget Year" });
+    data.push({ hierarchy: hierarchy.concat( "Cycle" ), responsible: "Budget Manager", form: "Title", desc: "BES or PB" });
+    data.push({ hierarchy: hierarchy.concat( "Title" ), responsible: "Budget Manager", form: "Title", desc: "A Title" });
+    data.push({ hierarchy: hierarchy.concat( "Community" ), responsible: "Budget Manager", form: "Title", desc: "CBDP" });
+    data.push({ hierarchy: hierarchy.concat( "Type" ), responsible: "Budget Manager", form: "Title", desc: "Type of JB" });
+    data.push({ hierarchy: hierarchy.concat( "Appropriation" ), responsible: "Budget Manager", form: "Title", desc: "RDT&E or PROC" });
+    data.push({ hierarchy: hierarchy.concat( "Overview" ), responsible: "Budget Manager", form: "Overview", desc: "A file with an overall desription" });
+    data.push({ hierarchy: hierarchy.concat( "R-1" ), responsible: "Budget Manager", form: "R-1", desc: "A file that is the R-1 form" });
     return data;
   }
 
-  getR2Fields( hier, item ) :any {
-
-    let data: any[] = [];
-
-    data.push({
-      hierarchy: hier.concat( "Overview" ),
-      responsible: "Budget Manager",
-      form: "R-2",
-      desc: "Overall Mission Description and Budget Item Justification"
-    });
-
-    data.push({
-      hierarchy: hier.concat( "Change Summary" ),
-      responsible: "Budget Manager",
-      form: "R-2",
-      desc: "Overall Program Change Summary ($ in Millions)"
-    });
+  getR2Fields( hierarchy: string[], item ) {
+    let data = [];
+    data.push({ hierarchy: hierarchy.concat( "Overview" ), responsible: "Budget Manager", form: "R-2", desc: "Overall Mission Description and Budget Item Justification" });
+    data.push({ hierarchy: hierarchy.concat( "Change Summary" ), responsible: "Budget Manager", form: "R-2", desc: "Overall Program Change Summary ($ in Millions)" });
     return data;
   }
 
-  getR2AFields(hier, item) :any {
-
-    let data: any[] = [];
-    data.push({
-      hierarchy: hier.concat("Mission Description"),
-      responsible: "Budget Manager",
-      form: "R-2A",
-      desc: "Item Mission Description and Budget Item Justification"
-    });
-
-    data.push({
-      hierarchy: hier.concat("Accomplishments"),
-      responsible: "Budget Manager",
-      form: "R-2A",
-      desc: "Accomplishments Overview"
-    });
-
-    data.push({
-      hierarchy: hier.concat("Program Accomplishments"),
-      responsible: "Program Managers",
-      form: "R-2A",
-      desc: "Accomplishments for each Program"
-    });
-
-
-    data.push({
-      hierarchy: hier.concat("Program Bullets"),
-      responsible: "Program Managers",
-      form: "R-2A",
-      desc: "Bullets from each Program"
-    });
+  getR2AFields(hierarchy: string[], item) {
+    let data = [];
+    data.push({ hierarchy: hierarchy.concat("Mission Description"), responsible: "Budget Manager", form: "R-2A", desc: "Item Mission Description and Budget Item Justification" });
+    data.push({ hierarchy: hierarchy.concat("Accomplishments"), responsible: "Budget Manager", form: "R-2A", desc: "Accomplishments Overview" });
+    data.push({ hierarchy: hierarchy.concat("Program Accomplishments"), responsible: "Program Managers", form: "R-2A", desc: "Accomplishments for each Program" });
+    data.push({ hierarchy: hierarchy.concat("Program Bullets"), responsible: "Program Managers", form: "R-2A", desc: "Bullets from each Program" });
 
     if ( item.ba != "6" )
-    data.push({
-      hierarchy: hier.concat("Other Funding"),
-      responsible: "Budget Manager",
-      form: "R-2A",
-      desc: "Other Program Funding for this Item"
-    });
-
-    data.push({
-      hierarchy: hier.concat("Remarks"),
-      responsible: "Budget Manager",
-      form: "R-2A",
-      desc: "Item Remarks"
-    });
+    data.push({ hierarchy: hierarchy.concat("Other Funding"), responsible: "Budget Manager", form: "R-2A", desc: "Other Program Funding for this Item" });
+    data.push({ hierarchy: hierarchy.concat("Remarks"), responsible: "Budget Manager", form: "R-2A", desc: "Item Remarks" });
 
     if ( item.ba == "4" || item.ba == "5" || item.ba == "7"   ){
-      data.push({
-        hierarchy: hier.concat("Acquisition Strategy"),
-        responsible: "Program Managers",
-        form: "R-2A",
-        desc: "Acquisition Strategy from each Program"
-      });
+      data.push({ hierarchy: hierarchy.concat("Acquisition Strategy"), responsible: "Program Managers", form: "R-2A", desc: "Acquisition Strategy from each Program" });
     }
 
     if ( item.ba == "7"){
-      data.push({
-        hierarchy: hier.concat("Performance Metrics"),
-        responsible: "Program Managers",
-        form: "R-2A",
-        desc: "Item Performance Metrics"
-      });
+      data.push({ hierarchy: hierarchy.concat("Performance Metrics"), responsible: "Program Managers", form: "R-2A", desc: "Item Performance Metrics" });
     }
     return data;
   }
 
-  getR3Fields(hier, item) :any {
-
-    let data:any[] = [];
-    data.push({
-      hierarchy: hier.concat("Product Development"),
-      responsible: "Program Managers",
-      form: "R-3",
-      desc: "Table of Contracts from each Program for Product Development ($ in Millions)"
-    });
-    data.push({
-      hierarchy: hier.concat("Support"),
-      responsible: "Program Managers",
-      form: "R-3",
-      desc: "Table of Contracts from each Program for Support ($ in Millions)"
-    });
-    data.push({
-      hierarchy: hier.concat("Test and Evaluation"),
-      responsible: "Program Managers",
-      form: "R-3",
-      desc: "Table of Contracts from each Program for Test and Evaluation ($ in Millions)"
-    });
-    data.push({
-      hierarchy: hier.concat("Management Services"),
-      responsible: "Program Managers",
-      form: "R-3",
-      desc: "Table of Contracts from each Program for Management Services ($ in Millions)"
-    });
-    data.push({
-      hierarchy: hier.concat("Remarks"),
-      responsible: "Budget Manager",
-      form: "R-3",
-      desc: "Item Remarks"
-    });
+  getR3Fields(hierarchy: string[], item) {
+    let data = [];
+    data.push({ hierarchy: hierarchy.concat("Product Development"), responsible: "Program Managers", form: "R-3", desc: "Table of Contracts from each Program for Product Development ($ in Millions)" });
+    data.push({ hierarchy: hierarchy.concat("Support"), responsible: "Program Managers", form: "R-3", desc: "Table of Contracts from each Program for Support ($ in Millions)" });
+    data.push({ hierarchy: hierarchy.concat("Test and Evaluation"), responsible: "Program Managers", form: "R-3", desc: "Table of Contracts from each Program for Test and Evaluation ($ in Millions)" });
+    data.push({ hierarchy: hierarchy.concat("Management Services"), responsible: "Program Managers", form: "R-3", desc: "Table of Contracts from each Program for Management Services ($ in Millions)" });
+    data.push({ hierarchy: hierarchy.concat("Remarks"), responsible: "Budget Manager", form: "R-3", desc: "Item Remarks" });
     return data;
   }
 
-  getR4Fields(hier, item) :any {
-    let data:any[] = [
-      {
-        hierarchy: hier.concat("Gantt Chart"),
-        responsible: "Budget Manager",
-        form: "R-4",
-        desc: "A generated Gantt Chart"
-      }
-    ];
-    return data;
+  getR4Fields(hierarchy: string[], item) {
+    return [{ hierarchy: hierarchy.concat("Gantt Chart"), responsible: "Budget Manager", form: "R-4", desc: "A generated Gantt Chart" } ];
   }
 
-  getR4AFields(hier, item) :any {
-    let data:any[] = [
-      {
-        hierarchy: hier.concat("Schedule Details"),
-        responsible: "Program Managers",
-        form: "R-4A",
-        desc: "Schedule Details from each program to generate a Gantt Chart"
-      }
-    ];
-    return data;
+  getR4AFields(hierarchy: string[], item) {
+    return [{ hierarchy: hierarchy.concat("Schedule Details"), responsible: "Program Managers", form: "R-4A", desc: "Schedule Details from each program to generate a Gantt Chart" } ];
   }
 
-  getDemoData() {
+  getItems() {
 
-    this.items = [
+    return [
       { inum: "LF1", ba: "1", pe: "0601384BP", fyt: 30 },
       { inum: "PS1", ba: "1", pe: "0601384BP", fyt: 16 },
 
