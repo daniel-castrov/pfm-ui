0ce5d95e702e6a09e4d98fe3fdcc34f3cc606757
daniel.castro@centally.com <castrov.daniel@gmail.com>
2019-01-03 11:39:11 -0500

    Added redirect to no access page when authorization fails | PEX-993

--- src/app/utils/can.activate.ts
+++ src/app/utils/can.activate.ts
@@ -1,14 +1,17 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
+import {ActivatedRouteSnapshot, Router, RouterStateSnapshot} from "@angular/router";
 import {Authorization, AuthorizationResult} from "../services/authorization";
 
 @Injectable()
 export class CanActivateAuth {
 
-  constructor( private authorization: Authorization ) {}
+  constructor( private authorization: Authorization, private router: Router ) {}
 
   async canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot ) {
       const auth = await this.authorization.forUrl(route.routeConfig.path);
+      if (auth !== AuthorizationResult.Ok) {
+        this.router.navigate(['/no-access']);
+      }
       return auth == AuthorizationResult.Ok;
   }
 
