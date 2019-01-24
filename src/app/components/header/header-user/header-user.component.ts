3455895aeca5e2b3a53cac1887f3d1b43cd51fb7
Gabrielle Campagna <gabycampagna@gmail.com>
2019-01-23 20:52:28 -0800

    Add user actions to User dropdown menu for User_Approver. Add My Actions for User_Approver. Add notifications for user actions.

--- src/app/components/header/header-user/header-user.component.ts
+++ src/app/components/header/header-user/header-user.component.ts
@@ -1,6 +1,9 @@
-import {Component, Input, ViewChild} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {ElevationService} from '../../../services/elevation.component';
 import {AuthUser} from '../../../generated/model/authUser';
+import {RequestsService} from '../../../services/requests.service';
+import {POMService} from '../../../generated/api/pOM.service';
+import {Pom} from '../../../generated/model/pom';
 import {MenuBarComponent} from "../../menu-bar/menu-bar.component";
 
 @Component({
@@ -12,9 +15,33 @@ export class HeaderUserComponent {
 
   @Input() isAuthenticated: boolean;
   @Input() authUser: AuthUser;
+  pomStatus: Pom.StatusEnum;
+
 
   @ViewChild(MenuBarComponent) menuBarComponent: MenuBarComponent;
 
-  constructor( public elevationService: ElevationService ) {}
+  requests: Request[];
+  prChangeNotifications: PrChangeNotification[];
+
+  constructor( private requestsService: RequestsService,
+                 public elevationService: ElevationService,
+                 private pomService: POMService ) {}
+
+   async ngOnInit() {
+     this.requests = await this.requestsService.getRequests().toPromise();
+     this.prChangeNotifications = (await this.prChangeNotificationsService.getByOrganization().toPromise()).result;
+     this.pomService.getByCommunityId(this.authUser.currentCommunity.id).subscribe(data => {
+       delete this.pomStatus;
+       data.result.forEach((p: Pom) => {
+         this.pomStatus = p.status;
+       });
+     });
+   }
+
+   refresh() {
+     this.ngOnInit();
+     this.userActionsComponent && this.userActionsComponent.ngOnInit();
+     this.prChangeNotificationsComponent && this.prChangeNotificationsComponent.ngOnInit();
+   }
 
 }
