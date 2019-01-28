9ba4995988df220d62abf86323697b4dd6ed87c2
Gabrielle Campagna <gabycampagna@gmail.com>
2019-01-27 20:45:28 -0800

    Implement ag-grid table for approve request component

--- src/app/components/user-management/approve-requests/approve-requests.component.ts
+++ src/app/components/user-management/approve-requests/approve-requests.component.ts
@@ -5,6 +5,7 @@ import { Request } from '../../../services/request';
 import { HeaderComponent } from '../../header/header.component';
 import { Injectables } from '../../../services/injectables';
 import { RequestsService } from '../../../services/requests.service';
+import { AgGridNg2 } from 'ag-grid-angular';
 import { PrChangeNotification, PrChangeNotificationsService } from "../../../generated";
 import { Notify } from '../../../utils/Notify';
 
@@ -16,9 +17,46 @@ import { Notify } from '../../../utils/Notify';
 export class ApproveRequestsComponent implements OnInit {
 
   @ViewChild(HeaderComponent) header: HeaderComponent;
+  @ViewChild("agGrid") private agGrid: AgGridNg2;
 
   requests: Request[];
   prChangeNotifications: PrChangeNotification[];
+
+  columnDefs = [
+    {
+      headerName: 'Date',
+      field: 'date'
+    },
+    {
+      headerName: 'Timestamp',
+      field: 'timestamp'
+    },
+    {
+      headerName: 'Type',
+      field: 'type'
+    },
+    {
+      headerName: 'Originator',
+      field: 'originator',
+      width: 280;
+    },
+    {
+      headerName: 'Approve',
+      field: 'approve'
+    },
+    {
+      headerName: 'Deny',
+      field: 'deny'
+    }
+     ];
+
+  rowData = [
+      {date: '3', timestamp: '12:00', type: 'Assign Role', originator: 'Bill', approve: 'button', deny: 'button' },
+      {date: '1', timestamp: '12:00', type: 'Assign Role', originator: 'Beth',  approve: 'button', deny: 'button' },
+      {date: '4', timestamp: '12:00', type: 'Assign Role', originator: 'Mary', approve: 'button', deny: 'button' },
+      {date: '24', timestamp: '12:00', type: 'Assign Role', originator: 'John', approve: 'button', deny: 'button' },
+      {date: '5', timestamp: '12:00', type: 'Assign Role', originator: 'Sally',  approve: 'button', deny: 'button' }
+     ];
 
   constructor( injectables: Injectables, // initilizes the static members on the class Injectables
                private requestsService: RequestsService,
@@ -52,4 +90,13 @@ export class ApproveRequestsComponent implements OnInit {
       Notify.exception(e.message);
     }
   }
+
+ onGridReadyOrgs(params) {
+   params.api.sizeColumnsToFit();
+   window.addEventListener("resize", function() {
+     setTimeout(() => {
+       params.api.sizeColumnsToFit();
+     });
+   });
+ }
 }
