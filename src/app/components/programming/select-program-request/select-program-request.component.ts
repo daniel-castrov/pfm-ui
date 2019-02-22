98c35b9dc12fd725074ade6988d6ef605f35eab6
swaroop.kaur <swaroop.kaur@p-exchange.com>
2019-02-22 15:34:47 -0500

    tooltip changed to show totals

--- src/app/components/programming/select-program-request/select-program-request.component.ts
+++ src/app/components/programming/select-program-request/select-program-request.component.ts
@@ -79,22 +79,33 @@ export class SelectProgramRequestComponent implements OnInit {
   }
 
   async loadChart(by:number) {
+    console.log('current year-'+by)
+    console.log('community toas-',this.pom.communityToas)
     this.populateRowData(by);
+    console.log('row data-',this.rowsData)
     const communityToas = this.pom.communityToas
     for(let i = 0; i < communityToas.length; i++) {
       let prop = communityToas[i].year.toString()
       let bar: any[] = []
       bar.push(prop)
-      let total = 0
+      let totalPrevious = ''
+      let totalCurrent = ''
       for(let j = 0; j < this.rowsData.length; j++) {
+        if(this.rowsData[j]['id'] == 'PB '+(by-2000-1)) {
+          totalPrevious = this.rowsData[j]['id'] + ': ' + this.rowsData[j]['total']
+        }
         if(this.rowsData[j]['id'] == 'POM '+(by-2000)+' TOA') {
-          total = this.rowsData[j][prop]
+          totalCurrent = this.rowsData[j]['id'] + ': ' + this.rowsData[j]['total']
         }
         if(!(this.rowsData[j]['id'] == 'PB '+(by-2000-1) || this.rowsData[j]['id'] == 'POM '+(by-2000)+' TOA')) {
           bar.push(this.rowsData[j][prop])
-          //bar.push('PB '+(by-2000-1)+' + '+'POM '+(by-2000)+' TOA')
-          //bar.push('PB '+(communityToas[i].year-2000-1)+' + '+'POM '+(communityToas[i].year-2000)+' TOA'+' = '+total)
-          bar.push(total)
+          bar.push(
+            totalPrevious 
+            +'\n'
+            +totalCurrent
+            +'\n'
+            +this.rowsData[j]['id'] + ': ' + this.rowsData[j]['total']
+          )
         }
       }
       this.charty.push(bar)
