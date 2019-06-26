414cc2a7f47642a32f36b96efa68dcd25207e640
mohini <mohini.mulik@coreviewsystems.com>
2019-06-26 16:54:06 +0530

    left side Menu bar added

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -15,12 +15,9 @@ import {UserActionsComponent} from "./user-actions/user-actions.component";
 import {PrChangeNotificationsComponent} from "./pr-change-notofications/pr-change-notifications.component";
 import { onMainContentChange ,onSideNavChange, animateText} from './animation/animation';
 import { MatSidenav, } from '@angular/material';
+import { UserUtils } from '../../services/user.utils';
+import { SidenavService } from './service/service';
 
-interface Page {
-  link: string;
-  name: string;
-  icon: string;
-}
 @Component({
   selector: 'menu-bar',
   templateUrl: './menu-bar.component.html',
@@ -28,20 +25,15 @@ interface Page {
   animations: [ onMainContentChange,onSideNavChange, animateText ]
 })
 export class MenuBarComponent implements OnInit {
-
+  disable : boolean = false;
+  enable : boolean = true;
   @Input() authUser: AuthUser;
   pomStatus: Pom.StatusEnum;
   public onSideNavChange: boolean;
-  @Input() sidenav: MatSidenav
+  @Input() sidenav: MatSidenav;
   
     public sideNavState: boolean = false;
     public linkText: boolean = false;
-  
-    public pages: Page[] = [
-      {name: 'Inbox', link:'some-link', icon: 'inbox'},
-      {name: 'Starred', link:'some-link', icon: 'star'},
-      {name: 'Send email', link:'some-link', icon: 'send'},
-    ]
 
   @ViewChild(ReportsMenuComponent) reportsMenuComponent: ReportsMenuComponent;
   @ViewChild(ProgrammingMenuComponent) programmingMenuComponent: ProgrammingMenuComponent;
@@ -52,10 +44,18 @@ export class MenuBarComponent implements OnInit {
   @ViewChild(AdminMenuComponent) adminMenuComponent: AdminMenuComponent;
   @ViewChild(UserActionsComponent) userActionsComponent: UserActionsComponent;
   @ViewChild(PrChangeNotificationsComponent) prChangeNotificationsComponent: PrChangeNotificationsComponent;
-
+  roles: string[];
+  
   constructor( private requestsService: RequestsService,
                public elevationService: ElevationService,
-               private pomService: POMService ) {}
+               private pomService: POMService ,
+               private userUtils: UserUtils,
+               private service : SidenavService,) {
+                this.service.sideNavState$.subscribe( res => {
+                  console.log(res)
+                  this.onSideNavChange = res;
+                })
+               }
 
   async ngOnInit() {
     this.pomService.getAll().subscribe(data => {
@@ -64,6 +64,7 @@ export class MenuBarComponent implements OnInit {
         this.pomStatus = p.status;
       });
     });
+    this.roles = await this.userUtils.roles().toPromise();
   }
 
   onSinenavToggle() {
@@ -72,7 +73,7 @@ export class MenuBarComponent implements OnInit {
     setTimeout(() => {
       this.linkText = this.sideNavState;
     }, 200)
-    // this._sidenavService.sideNavState$.next(this.sideNavState)
+    this.service.sideNavState$.next(this.sideNavState)
   }
 
   refresh() {
