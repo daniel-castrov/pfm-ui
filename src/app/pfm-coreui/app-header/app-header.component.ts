143ada122a5101e8caeec96fe774769bc0d67617
mightywebb <burgess.webb@gmail.com>
2019-11-11 08:20:48 -0500

    app header name fix

--- src/app/pfm-coreui/app-header/app-header.component.ts
+++ src/app/pfm-coreui/app-header/app-header.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AppModel } from '../../../../projects/shared/src/lib/models/AppModel';
 
 @Component({
-  selector: 'app-app-header',
+  selector: 'pfm-app-header',
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.scss']
 })
