c647d9c651d030bb085e4d3f1c01ac03a6cd4425
Natalie Lockhart <natalie.lockhart@p-exchange.com>
2019-11-13 08:45:26 -0700

    PFM-64 - Adding PR Status demo widget setup

---
+++ src/app/components/home/dashboard/widgets/demo-widget-pr-status/demo-widget-pr-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-widget-pr-status',
+  templateUrl: './demo-widget-pr-status.component.html',
+  styleUrls: ['./demo-widget-pr-status.component.scss']
+})
+export class DemoWidgetPrStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
