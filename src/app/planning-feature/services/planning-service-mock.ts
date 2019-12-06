0815f88fed9a1206db0d2dd2c156737192f87bd2
mightywebb <burgess.webb@gmail.com>
2019-12-05 19:08:07 -0500

    better mock data, grid drag styling

--- src/app/planning-feature/services/planning-service-mock.ts
+++ src/app/planning-feature/services/planning-service-mock.ts
@@ -7,6 +7,7 @@ import { MissionAttachment } from '../models/MissionAttachment';
 
 export class PlanningServiceMock extends PlanningService{
 
+  sampleData:string[] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec.'.split('.');
   constructor(){
     super(null);
   }
@@ -31,8 +32,8 @@ export class PlanningServiceMock extends PlanningService{
       for (let i = 0; i < 25; i++) {
         let data: MissionPriority = new MissionPriority();
         data.priority = i + 1;
-        data.title = "Enable Nuclear Deterrent for " + year;
-        data.description = "Enable a safe, secure, reliable & effective Nuclear Deterrent";
+        data.title = this.sampleData[i] + year;
+        data.description = this.sampleData[this.sampleData.length - 1 - i];
         data.actions = new MissionAction();
         data.actions.canDelete = true;
         data.actions.canEdit = true;
