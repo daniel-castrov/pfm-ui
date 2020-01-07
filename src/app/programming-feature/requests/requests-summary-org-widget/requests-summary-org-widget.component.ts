4737292c47879bbc417b13929bb8f28bfe6bc82a
Scott Hall <scott.hall.oh@gmail.com>
2020-01-07 15:31:45 -0500

    Added dropdown, stubbed switching functionality
    Drop down is not visible due to css.
    Stub work.

--- src/app/programming-feature/requests/requests-summary-org-widget/requests-summary-org-widget.component.ts
+++ src/app/programming-feature/requests/requests-summary-org-widget/requests-summary-org-widget.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces';
 import { ProgramRequestForPOM } from '../../models/ProgramRequestForPOM';
+import { ListItem } from '../../../pfm-common-models/ListItem';
 
 @Component({
   selector: 'pfm-requests-summary-org-widget',
@@ -12,33 +13,29 @@ export class RequestsSummaryOrgWidgetComponent  {
   @Input() griddata:ProgramRequestForPOM[];
 
   chartReady:boolean;
+  availableCharts: ListItem[];
 
   public treeMapChart: any =  {
     chartType: 'TreeMap',
     dataTable: [
-      ['Program',  'Organization',   'Health',   'Demands'],
-      ['All',       null,             0,          0],
-      ['JSTO-CBD', 'All', 0, 0],
-      ['JPEO-CBRND', 'All', 0, 0],
-      ['JRO-CBD', 'All', 0, 0],
-      ['PAIDO-CBD', 'All', 0, 0],
-      ['DUS', 'All', 0, 0],
-      ['SPU',      'JSTO-CBD',     100,        50],
-      ['LDN',      'JSTO-CBD',     10,         0],
-      ['RUI',    'JPEO-CBRND',   99,         48],
-      ['PIP',       'JRO-CBD',      0,          10],
-      ['QPM',     'PAIDO-CBD',    70,         0],
-      ['WES',           'DUS',          0,          60]
+      ['Program', 'Organization', 'Health', 'Demands'],
+      ['Organization', null,  0,  0],
+      ['JSTO-CBD', 'Organization',  0,  0],
+      ['JPEO-CBRND', 'Organization',  0,  0],
+      ['JRO-CBD', 'Organization',  0, 0],
+      ['PAIDO-CBD', 'Organization',  0,  0],
+      ['DUS', 'Organization', 0, 0],
+      ['SPU', 'JSTO-CBD', 100, 50],
+      ['LDN', 'JSTO-CBD', 10, 0],
+      ['RUI', 'JPEO-CBRND', 99, 48],
+      ['PIP', 'JRO-CBD', 0, 10],
+      ['QPM', 'PAIDO-CBD', 70, 0],
+      ['WES', 'DUS', 0, 60]
     ],
     options: {
       title: 'Organization',
       width: 100,
       height: 100,
-      animation: {
-        duration: 1000,
-        easing: 'out',
-        startup: true
-      }
     }
   };
 
@@ -59,7 +56,23 @@ export class RequestsSummaryOrgWidgetComponent  {
   }
 
   ngOnInit() {
-
+    let chartOptions: string[] = ['Organization', 'BA Line', 'Program Status'];
+    this.availableCharts = this.toListItem(chartOptions);
   }
 
+  private chartSelected(){
+    //todo
+  }
+
+  private toListItem(years:string[]):ListItem[]{
+    let items:ListItem[] = [];
+    for(let year of years){
+      let item:ListItem = new ListItem();
+      item.id = year;
+      item.name = year;
+      item.value = year;
+      items.push(item);
+    }
+    return items;
+  }
 }
