7d171940815b0552e09a6c8ec7cda9fad44ef132
Vasantha Gajula <vasantha.gajula@p-exchange.com>
2020-01-24 16:20:01 -0500

    create pom  to database changes

--- src/app/programming-feature/services/pom-service.ts
+++ src/app/programming-feature/services/pom-service.ts
@@ -1,6 +1,7 @@
 import {Observable} from 'rxjs';
 import {BaseRestService} from '../../services/base-rest.service';
 import {HttpClient} from '@angular/common/http';
+import { Pom } from '../models/Pom';
 
 
 export abstract class PomService extends BaseRestService {
@@ -16,4 +17,6 @@ export abstract class PomService extends BaseRestService {
     abstract getPomFromFile(fileId: string): Observable<Object>;
 
     abstract getLatestPom(): Observable<object>;
+
+    abstract createPom(year:number,pom:Pom):Observable<Object>;
 }
