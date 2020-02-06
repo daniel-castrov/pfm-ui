1ed98897b64bcd2c4387e20a9df01eb8bec7dc32
Scott Hall <scott.hall.oh@gmail.com>
2020-02-06 12:53:49 -0500

    PFM-306 No data message
    Now when there is no data for chart the chart displays a message.

--- src/app/programming-feature/requests/requests-summary/requests-summary-toa-widget/requests-summary-toa-widget.component.ts
+++ src/app/programming-feature/requests/requests-summary/requests-summary-toa-widget/requests-summary-toa-widget.component.ts
@@ -20,15 +20,7 @@ export class RequestsSummaryToaWidgetComponent implements OnInit {
 
   public columnChart: any = {
     chartType: 'ColumnChart',
-    dataTable: [
-      ['Fiscal Year', 'PRs Submitted', 'PRs Planned', 'TOA Difference', 'Unallocated'],
-      ['FY16', 350001, 200001, 100000, 10000],
-      ['FY17', 400001, 300001, 150000, 200000],
-      ['FY18', 250001, 400001, 40000, 300000],
-      ['FY19', 650001, 6000, 300000, 100000],
-      ['FY20', 500001, 70000, 100000, 50000],
-      ['FY21', 600000, 200001, 200000, 300000],
-    ],
+    dataTable: [],
     options: {
       title: 'Community TOA',
       width: 200,
