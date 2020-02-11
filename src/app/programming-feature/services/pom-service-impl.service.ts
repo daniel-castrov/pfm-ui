ab937eea18a4445a89d1a15b1bf34e29c4211362
Michael Crump <michael.crump@p-exchange.com>
2020-02-11 00:36:28 -0500

    PFM-338 Hook up service endpoint for retrieving pom by year

--- src/app/programming-feature/services/pom-service-impl.service.ts
+++ src/app/programming-feature/services/pom-service-impl.service.ts
@@ -1,37 +1,40 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
-import {PomService} from './pom-service';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PomService } from './pom-service';
 import { Pom } from '../models/Pom';
 
 
-@Injectable({
-    providedIn: 'root'
-})
+@Injectable( {
+  providedIn: 'root'
+} )
 export class PomServiceImpl extends PomService {
 
-    constructor(protected httpClient: HttpClient) {
-        super(httpClient);
-    }
+  constructor( protected httpClient: HttpClient ) {
+    super( httpClient );
+  }
 
-    pBYearExists(year: string): Observable<Object> {
-        return this.get('pom/init/fromPB/year/' + year + '/exists');
-    }
+  pBYearExists( year: string ): Observable<object> {
+    return this.get( 'pom/init/fromPB/year/' + year + '/exists' );
+  }
 
-    getPomFromPb(): Observable<Object> {
-        return this.get('pom/init/fromPB');
-    }
+  getPomFromPb(): Observable<object> {
+    return this.get( 'pom/init/fromPB' );
+  }
 
-    getPomFromFile(fileId: string): Observable<Object> {
-        return this.get('pom/init/fromLibrary/id/' + fileId);
-    }
+  getPomFromFile( fileId: string ): Observable<object> {
+    return this.get( 'pom/init/fromLibrary/id/' + fileId );
+  }
 
-    getLatestPom(): Observable<object> {
-        return this.get('pom/latest');
-    }
+  getLatestPom(): Observable<object> {
+    return this.get( 'pom/latest' );
+  }
 
-    createPom(year:number,pom:Pom): Observable<object> {
-        return this.post('pom/year/'+ year,pom);
-    }
+  createPom( year: number, pom: Pom ): Observable<object> {
+    return this.post( 'pom/year/' + year, pom );
+  }
 
+  getPomForYear( year: number ): Observable<object> {
+    return this.get( 'pom/year' + year );
+  }
 }
--- src/app/programming-feature/services/pom-service.ts
+++ src/app/programming-feature/services/pom-service.ts
@@ -1,22 +1,24 @@
-import {Observable} from 'rxjs';
-import {BaseRestService} from '../../services/base-rest.service';
-import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BaseRestService } from '../../services/base-rest.service';
+import { HttpClient } from '@angular/common/http';
 import { Pom } from '../models/Pom';
 
 
 export abstract class PomService extends BaseRestService {
 
-    constructor(protected httpClient: HttpClient) {
-        super(httpClient);
-    }
+  constructor( protected httpClient: HttpClient ) {
+    super( httpClient );
+  }
 
-    abstract pBYearExists(year: string): Observable<Object>;
+  abstract pBYearExists( year: string ): Observable<object>;
 
-    abstract getPomFromPb(): Observable<Object>;
+  abstract getPomFromPb(): Observable<object>;
 
-    abstract getPomFromFile(fileId: string): Observable<Object>;
+  abstract getPomFromFile( fileId: string ): Observable<object>;
 
-    abstract getLatestPom(): Observable<object>;
+  abstract getLatestPom(): Observable<object>;
 
-    abstract createPom(year:number,pom:Pom):Observable<Object>;
+  abstract createPom( year: number, pom: Pom ): Observable<object>;
+
+  abstract getPomForYear( year: number ): Observable<object>;
 }
