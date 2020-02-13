2f7b0354e60bd2d6d95e55de367c9bfa526e400f
Scott Hall <scott.hall.oh@gmail.com>
2020-02-13 13:58:20 -0500

    PFM-305 Fix click functionality
    Fix click functionality

--- src/app/programming-feature/requests/requests-summary/requests-summary-org-widget/requests-summary-org-widget.component.ts
+++ src/app/programming-feature/requests/requests-summary/requests-summary-org-widget/requests-summary-org-widget.component.ts
@@ -1,17 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ProgramSummary } from '../../../models/ProgramSummary';
 import { ListItem } from '../../../../pfm-common-models/ListItem';
 import { Organization } from 'src/app/pfm-common-models/Organization';
 import { Role } from 'src/app/pfm-common-models/Role';
 import { RequestSummaryNavigationHistoryService } from '../requests-summary-navigation-history.service';
-//import { runInThisContext } from 'vm';
+// import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'pfm-requests-summary-org-widget',
   templateUrl: './requests-summary-org-widget.component.html',
   styleUrls: ['./requests-summary-org-widget.component.scss']
 })
-export class RequestsSummaryOrgWidgetComponent {
+export class RequestsSummaryOrgWidgetComponent implements OnInit {
 
   @Input() griddata: ProgramSummary[];
   @Input() orgs: Organization[];
@@ -21,7 +21,7 @@ export class RequestsSummaryOrgWidgetComponent {
   availableCharts: ListItem[];
   defaultChart: ListItem;
 
-  public treeMapChart: any = {
+  treeMapChart: any = {
     chartType: 'TreeMap',
     dataTable: [
       ['Program', 'Organization', 'Health', 'Demands'],
@@ -45,7 +45,7 @@ export class RequestsSummaryOrgWidgetComponent {
       maxColor: '#21809C',
       fontColor: 'black',
 
-      //showScale: true,
+      // showScale: true,
       showTooltips: true,
       width: 200,
       height: 200,
@@ -73,9 +73,14 @@ export class RequestsSummaryOrgWidgetComponent {
   }
 
   ngOnInit() {
-    let chartOptions: string[] = ['Organization', 'BA Line', 'Program Status'];
+    const chartOptions: string[] = ['Organization', 'BA Line', 'Program Status'];
     this.availableCharts = this.toListItem(chartOptions);
     this.defaultChart = this.availableCharts[0];
+  }
+
+  noClick(event) {
+    console.log(event);
+    console.log('click');
   }
 
   loadPreviousSelection() {
@@ -87,48 +92,33 @@ export class RequestsSummaryOrgWidgetComponent {
         this.defaultChart = currentOrganizationWidget;
       }
     }
-    this.requestSummaryNavigationHistoryService.updateRequestSummaryNavigationHistory({ selectedOrganizationWidget: this.defaultChart.id });
+    this.requestSummaryNavigationHistoryService.updateRequestSummaryNavigationHistory(
+      { selectedOrganizationWidget: this.defaultChart.id }
+      );
   }
 
-  public chartSelected(chartType: any) {
-    if (chartType.id === "Organization") {
-      //change to org
+  chartSelected(chartType: any) {
+    if (chartType.id === 'Organization') {
+      // change to org
       this.defaultChart = this.availableCharts[0];
       this.chartOrganization();
-    }
-    else if (chartType.id === "BA Line") {
-      //change to ba line
+    } else if (chartType.id === 'BA Line') {
+      // change to ba line
       this.defaultChart = this.availableCharts[1];
       this.chartBALine();
-    }
-    else if (chartType.id === "Program Status") {
-      //change to program status
+    } else if (chartType.id === 'Program Status') {
+      // change to program status
       this.defaultChart = this.availableCharts[2];
       this.chartProgramStatus();
     }
-    this.requestSummaryNavigationHistoryService.updateRequestSummaryNavigationHistory({ selectedOrganizationWidget: this.defaultChart.id });
+    this.requestSummaryNavigationHistoryService.updateRequestSummaryNavigationHistory(
+      { selectedOrganizationWidget: this.defaultChart.id }
+      );
   }
 
   private chartOrganization() {
-    //set up Organization tree structure
-
-    /*let organizationTable = [
-      ['Program', 'Organization', 'Health', 'Demands'],
-      ['Organization', null, 0, 0],
-      ['JSTO-CBD', 'Organization', 0, 0],
-      ['JPEO-CBRND', 'Organization', 0, 0],
-      ['JRO-CBD', 'Organization', 0, 0],
-      ['PAIDO-CBD', 'Organization', 0, 0],
-      ['DUS', 'Organization', 0, 0],
-      ['SPU', 'JSTO-CBD', 100, 50],
-      ['LDN', 'JSTO-CBD', 10, 0],
-      ['RUI', 'JPEO-CBRND', 99, 48],
-      ['PIP', 'JRO-CBD', 10, 10],
-      ['QPM', 'PAIDO-CBD', 70, 0],
-      ['WES', 'DUS', 10, 60]
-    ];*/
-
-    //load data into chart
+    // set up Organization tree structure
+    // load data into chart
     let orgDataTable: any;
 
     orgDataTable = [];
@@ -140,26 +130,28 @@ export class RequestsSummaryOrgWidgetComponent {
       }
     });
 
-    var toltaltfunding: number = 0;
+    let toltaltfunding = 0;
     if (this.griddata) {
       this.griddata.forEach(ps => {
         toltaltfunding = toltaltfunding + ps.fundsTotal;
       });
 
       this.griddata.forEach(ps => {
-        let orgName = this.getOrgName(ps.organiztionId);
-        orgDataTable.push([ps.programName, orgName, ((ps.fundsTotal / toltaltfunding) * 100), this.getOrgColors(orgName)]);
+        const orgName = this.getOrgName(ps.organiztionId);
+        orgDataTable.push(
+          [ps.programName, orgName, ((ps.fundsTotal / toltaltfunding) * 100), this.getOrgColors(orgName)]
+        );
       });
-      //set data to chart
-      this.treeMapChart.dataTable = orgDataTable; //organizationTable;
+      // set data to chart
+      this.treeMapChart.dataTable = orgDataTable; // organizationTable;
       this.treeMapChart = Object.assign({}, this.treeMapChart);
       // this.treeMapChart.component.draw();
     }
   }
 
   private chartBALine() {
-    //set up BA Line tree structure
-    let lineTable = [
+    // set up BA Line tree structure
+    const lineTable = [
       ['Program', 'Organization', 'Health', 'Demands'],
       ['BA Line', null, 0, 0],
       ['SA0001', 'BA Line', 0, 0],
@@ -180,57 +172,44 @@ export class RequestsSummaryOrgWidgetComponent {
       ['Test8', 'PHM001', 50, 10]
     ];
 
-    //load data into chart
+    // load data into chart
 
-    //set data to chart
+    // set data to chart
     this.treeMapChart.dataTable = lineTable;
     setTimeout(() => {
-      //this.treeMapChart.component.draw();
+      // this.treeMapChart.component.draw();
       this.treeMapChart = Object.assign({}, this.treeMapChart);
     }, 0);
-    
+
   }
 
   private chartProgramStatus() {
-    //set up Status tree structure
-    /*
-     let statusTable = [
-       ['Program', 'Program Status', 'Health', 'Demands'],
-       ['Program Status', null,  0,  0],
-       ['pfm', 'Program Status',  0,  0],
-       ['pfm1', 'Program Status',  0,  0],
-       ['pfm2', 'Program Status',  0,  0],
-       ['OUTSTANDING', 'pfm', 50, 100],
-       ['saved', 'pfm', 30, 40],
-       ['approved', 'pfm', 50, 20]
-     ];
-     */
-
-
-
+    // set up Status tree structure
     let statusTable: any;
 
     statusTable = [];
     statusTable.push(['Program', 'Program Status', 'Health', 'Demands']);
     statusTable.push(['Program Status', null, 0, 0]);
 
-    //statusTable.push([ 'Outstanding','Program Status',0,0]);
+    // statusTable.push([ 'Outstanding','Program Status',0,0]);
     /*this.roles.forEach(role =>{
          let roleacrynm = this.getRoleAcrynm(role.name);
         statusTable.push([ roleacrynm,'Program Status',0,0]);
     });
     */
-    var toltalNoOfPrograms: number = this.griddata.length;
+    const toltalNoOfPrograms: number = this.griddata.length;
     if (this.griddata.length > 0) {
-      let assignedTo = this.griddata[0].assignedTo;
-      let roleacrynm = this.getRoleAcrynm(assignedTo);
+      const assignedTo = this.griddata[0].assignedTo;
+      const roleacrynm = this.getRoleAcrynm(assignedTo);
 
       // get the count for each of the status
-      let approvedcount: number = 0, savedcount: number = 0;
-      let outstandingcount: number = 0, rejectedcount: number = 0;
+      let approvedcount = 0;
+      let savedcount = 0;
+      let outstandingcount = 0;
+      let rejectedcount = 0;
 
       this.griddata.forEach(ps => {
-        if (ps.assignedTo == assignedTo) {
+        if (ps.assignedTo === assignedTo) {
           switch (ps.status.toLowerCase()) {
             case 'approved':
               approvedcount = approvedcount + 1;
@@ -248,12 +227,12 @@ export class RequestsSummaryOrgWidgetComponent {
         }
       });
 
-      let outstanding = ((outstandingcount * 100) / toltalNoOfPrograms);
-      let saved = ((savedcount * 100) / toltalNoOfPrograms);
-      let rejected = ((rejectedcount * 100) / toltalNoOfPrograms);
-      let approved = ((approvedcount * 100) / toltalNoOfPrograms);
+      const outstanding = ((outstandingcount * 100) / toltalNoOfPrograms);
+      const saved = ((savedcount * 100) / toltalNoOfPrograms);
+      const rejected = ((rejectedcount * 100) / toltalNoOfPrograms);
+      const approved = ((approvedcount * 100) / toltalNoOfPrograms);
 
-      //assignedTo = assignedTo.replace('_',' ');
+      // assignedTo = assignedTo.replace('_',' ');
       statusTable.push([roleacrynm + ' : Outstanding', 'Program Status', outstanding, 100]);
       statusTable.push([roleacrynm + ' : Saved', 'Program Status', saved, 50]);
       statusTable.push([roleacrynm + ' : Approved', 'Program Status', approved, 70]);
@@ -261,20 +240,20 @@ export class RequestsSummaryOrgWidgetComponent {
     }
 
     console.log(JSON.stringify(statusTable));
-    //load data into chart
+    // load data into chart
 
-    //set data to chart
+    // set data to chart
     this.treeMapChart.dataTable = statusTable;
     this.treeMapChart = Object.assign({}, this.treeMapChart);
-    //this.treeMapChart.component.draw();
-    //this.treeMapChart.goUpAndDraw();
+    // this.treeMapChart.component.draw();
+    // this.treeMapChart.goUpAndDraw();
 
   }
 
   private toListItem(years: string[]): ListItem[] {
-    let items: ListItem[] = [];
-    for (let year of years) {
-      let item: ListItem = new ListItem();
+    const items: ListItem[] = [];
+    for (const year of years) {
+      const item: ListItem = new ListItem();
       item.id = year;
       item.name = year;
       item.value = year;
@@ -284,9 +263,9 @@ export class RequestsSummaryOrgWidgetComponent {
   }
 
   getOrgName(orgId: string): string {
-    var orgName = "";
-    for (let org of this.orgs) {
-      if (org.id == orgId) {
+    let orgName = '';
+    for (const org of this.orgs) {
+      if (org.id === orgId) {
         orgName = org.abbreviation;
         break;
       }
@@ -295,10 +274,10 @@ export class RequestsSummaryOrgWidgetComponent {
   }
 
   hasOrgData(orgId: string): boolean {
-    let hasData: boolean = false;
+    let hasData = false;
     if (this.griddata) {
-      for (let ps of this.griddata) {
-        if (ps.organiztionId == orgId) {
+      for (const ps of this.griddata) {
+        if (ps.organiztionId === orgId) {
           hasData = true;
           break;
         }
@@ -309,12 +288,12 @@ export class RequestsSummaryOrgWidgetComponent {
 
   getOrgColors(orgName: string): string {
     let colorcode: string;
-    let orgcolors = []
-    orgcolors["PAIO"] = "60";
-    orgcolors["DUSA"] = "0";
-    orgcolors["JSTO"] = "40";
-    orgcolors["JPEO"] = "10";
-    orgcolors["JRO"] = "90";
+    const orgcolors = [];
+    orgcolors['PAIO'] = '60';
+    orgcolors['DUSA'] = '0';
+    orgcolors['JSTO'] = '40';
+    orgcolors['JPEO'] = '10';
+    orgcolors['JRO'] = '90';
 
     orgName = orgName.split('-')[0];
     colorcode = orgcolors[orgName];
@@ -322,18 +301,18 @@ export class RequestsSummaryOrgWidgetComponent {
   }
 
   getRoleAcrynm(role: string): string {
-    let acrynm: string = '';
+    let acrynm = '';
     role = role.replace('_', ' ');
-    let names: string[] = role.split(' ');
-    if (names.length == 1) {
+    const names: string[] = role.split(' ');
+    if (names.length === 1) {
       acrynm = role;
-    }
-    else {
+    } else {
       names.forEach(nm => {
-        if (nm.indexOf('POM') != -1)
+        if (nm.indexOf('POM') !== -1) {
           acrynm = acrynm + 'PM';
-        else
+        } else {
           acrynm = acrynm + nm.substr(0, 1);
+        }
       });
     }
     console.log(role + ' : ' + acrynm);
