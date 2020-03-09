b2b4faaa62b51e68b2e107338bafafd37af33505
osirispujols <osirispujols@gmail.com>
2020-03-09 12:04:26 -0400

    Retrieves Security Def LOE from server. Retrieves Strategic Imperative from server. Retrieves Agency Objective from server.

--- src/app/programming-feature/models/Program.ts
+++ src/app/programming-feature/models/Program.ts
@@ -1,44 +1,47 @@
-import {FundingLine} from './FundingLine';
-import {Milestone} from './Milestone';
+import { FundingLine } from './FundingLine';
+import { Milestone } from './Milestone';
 
 export class Program {
-    created: any;
-    createdBy: string;
-    modified: any;
-    modifiedBy: string;
+  created: any;
+  createdBy: string;
+  modified: any;
+  modifiedBy: string;
 
-    emphases: Array<string>;
+  emphases: Array<string>;
 
-    id: string;
-    organizationId: string;
-    programStatus: string;
-    bulkOrigin: boolean;
-    type: string;
-    containerId: string;
-    description: string;
-    shortName: string;
-    longName: string;
-    imageName: string;
-    imageArea: string;
-    infolink: string;
-    justification?: string;
-    impactN: string;
-    execution: string;
-    fundingLines: Array<FundingLine>;
-    milestones: Array<Milestone>;
-    primaryCapability: string;
-    coreCapability: string;
-    secondaryCapability: string;
-    functionalArea: string;
-    medicalArea: string;
-    nbcCategory: string;
-    commodityArea: string;
-    bsvStrategy: string;
-    dasdCbd: string;
-    leadComponent: string;
-    manager: string;
-    acquisitionType?: string;
-    responsibleRoleId: string;
+  id: string;
+  organizationId: string;
+  programStatus: string;
+  bulkOrigin: boolean;
+  type: string;
+  containerId: string;
+  description: string;
+  shortName: string;
+  longName: string;
+  imageName: string;
+  imageArea: string;
+  infolink: string;
+  justification?: string;
+  impactN: string;
+  execution: string;
+  fundingLines: Array<FundingLine>;
+  milestones: Array<Milestone>;
+  primaryCapability: string;
+  coreCapability: string;
+  secondaryCapability: string;
+  functionalArea: string;
+  medicalArea: string;
+  nbcCategory: string;
+  commodityArea: string;
+  bsvStrategy: string;
+  dasdCbd: string;
+  leadComponent: string;
+  manager: string;
+  acquisitionType?: string;
+  responsibleRoleId: string;
+  secDefLOE: string;
+  strategicImperative: string;
+  agencyObjective: string;
 
-    constructor() {}
+  constructor() { }
 }
