67edfe4859e8e99087cfcda5c22d65bc02065475
Enmanuel Reyes <EnmanuelReyes@live.com>
2020-03-24 11:49:33 -0400

    Downgrade primeng version Import Primeng ToastModule

--- src/app/pfm-coreui/app-header/app-header.component.ts
+++ src/app/pfm-coreui/app-header/app-header.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { AppModel } from '../../pfm-common-models/AppModel';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'pfm-app-header',
   templateUrl: './app-header.component.html',
-  styleUrls: ['./app-header.component.scss']
+  styleUrls: ['./app-header.component.scss'],
+  providers: [MessageService]
 })
 export class AppHeaderComponent implements OnInit {
 
-  constructor(public appModel:AppModel) { }
+  constructor(public appModel: AppModel, private messageService: MessageService) {
+  }
 
-  //TODO - fix the menu drop down on the header - conflict with materail vs bootstrap
+  // TODO - fix the menu drop down on the header - conflict with materail vs bootstrap
   ngOnInit() {
   }
 
