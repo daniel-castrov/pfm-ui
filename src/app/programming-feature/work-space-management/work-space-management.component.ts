796e8ab3f8a8f7e873a67f7a9a0dcbad2fd607be
ariendy <ariendyjav@gmail.com>
2020-05-05 09:55:38 -0400

    Add workspace page top elements

--- src/app/programming-feature/work-space-management/work-space-management.component.ts
+++ src/app/programming-feature/work-space-management/work-space-management.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormatterUtil } from 'src/app/util/formatterUtil';
 
 @Component({
   selector: 'pfm-programming',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-space-management.component.scss']
 })
 export class WorkSpaceManagementComponent implements OnInit {
+  byYear: number;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.byYear = FormatterUtil.getCurrentFiscalYear() + 2;
+  }
+
+  compareVersion() {}
 }
