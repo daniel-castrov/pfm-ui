e5b3b63d136aef8357896e1c3ee697710d00b945
osirispujols <osirispujols@gmail.com>
2020-05-27 08:06:49 -0400

    Fix issue with user created Funding Line.

--- src/app/programming-feature/requests/requests-details/requests-funding-line-grid/requests-funding-line-grid.component.ts
+++ src/app/programming-feature/requests/requests-details/requests-funding-line-grid/requests-funding-line-grid.component.ts
@@ -988,19 +988,22 @@ export class RequestsFundingLineGridComponent implements OnInit {
   private validateNonSummaryRowData(rowIndex: any) {
     const row = this.nonSummaryFundingLineRows[rowIndex];
     let errorMessage = '';
-    if (!row.appropriation && (!row.appropriation?.length || row.appropriation?.toLowerCase() === 'select')) {
+    if (!row.appropriation || !row.appropriation?.length || row.appropriation?.toLowerCase() === 'select') {
       errorMessage = 'Please, select an APPN.';
-    } else if (!row.baOrBlin && (!row.baOrBlin?.length || row.baOrBlin?.toLowerCase() === 'select')) {
+    } else if (!row.baOrBlin || !row.baOrBlin?.length || row.baOrBlin?.toLowerCase() === 'select') {
       errorMessage = 'Please, select a BA/BLIN.';
-    } else if (!row.sag && (!row.sag?.length || row.sag?.toLowerCase() === 'select')) {
+    } else if (row.userCreated) {
+      if (!row.sag || !row.sag?.length || row.sag?.toLowerCase() === 'select') {
       errorMessage = 'Please, select a SAG.';
-    } else if (!row.wucd && (!row.wucd?.length || row.wucd?.toLowerCase() === 'select')) {
+      } else if (!row.wucd || !row.wucd?.length || row.wucd?.toLowerCase() === 'select') {
       errorMessage = 'Please, select a WUCD.';
     } else if (
-      !row.expenditureType &&
-      (!row.expenditureType?.length || row.expenditureType?.toLowerCase() === 'select')
+        !row.expenditureType ||
+        !row.expenditureType?.length ||
+        row.expenditureType?.toLowerCase() === 'select'
     ) {
       errorMessage = 'Please, select a EXP Type.';
+      }
     } else if (
       this.nonSummaryFundingLineRows.some((fundingLine, idx) => {
         return (
