a4c08a4e8b6a4f293cd7aa4c96d8ae93a585a187
osirispujols <osirispujols@gmail.com>
2020-05-28 16:47:45 -0400

    Add CTC field to Funding Line History.

--- src/app/programming-feature/requests/requests-details/requests-funding-line-grid/requests-funding-line-grid.component.ts
+++ src/app/programming-feature/requests/requests-details/requests-funding-line-grid/requests-funding-line-grid.component.ts
@@ -1985,6 +1985,20 @@ export class RequestsFundingLineGridComponent implements OnInit {
             minWidth: 75,
             maxWidth: 75,
             valueFormatter: params => this.currencyFormatter(params.data[params.colDef.field])
+          },
+          {
+            editable: false,
+            suppressMovable: true,
+            filter: false,
+            sortable: false,
+            suppressMenu: true,
+            field: 'ctc',
+            headerName: 'CTC',
+            cellClass: params => ['numeric-class'],
+            cellStyle: { display: 'flex', 'align-items': 'center', 'justify-content': 'flex-end' },
+            minWidth: 75,
+            maxWidth: 75,
+            valueFormatter: params => this.currencyFormatter(params.data[params.colDef.field])
           }
         ],
         defaultColDef: { flex: 1 }
@@ -2006,7 +2020,8 @@ export class RequestsFundingLineGridComponent implements OnInit {
             updatedDate: formatDate(fundingLineHistory.created, 'MM/d/yyyy H:mm', 'en-US'),
             reason: fundingLineHistory.reason,
             updatedBy: this.usersFullNameMap[fundingLineHistory.createdBy],
-            byTotal: total
+            byTotal: total,
+            ctc: fundingLineHistory.ctc
           };
           Object.assign(fundingLineHistoryEntry, ...byFields);
           fundingLineMasterDetail.push(fundingLineHistoryEntry);
