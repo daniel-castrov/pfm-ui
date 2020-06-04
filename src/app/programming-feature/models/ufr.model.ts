bddc3da155969ecc2ecef9aa18b2667f7d25e32d
osirispujols <osirispujols@gmail.com>
2020-06-04 17:25:55 -0400

    Allow create new UFR

--- src/app/programming-feature/models/ufr.model.ts
+++ src/app/programming-feature/models/ufr.model.ts
@@ -1,36 +1,16 @@
 import { UFRStatus } from './enumerations/ufr-status.model';
 import { ShortyType } from './enumerations/shorty-type.model';
 import { Disposition } from './enumerations/disposition.model';
+import { Program } from './Program';
 
-export class UFR {
+export class UFR extends Program {
   id: string;
-  requestNumber: number;
-  ufrName: string;
-  notes: string;
-  disposition: Disposition;
-  dispositionExplanation: string;
-  ufrStatus: UFRStatus;
-  shortyType: ShortyType;
-
-  created?: any;
-  modified?: any;
-  modifiedBy?: string;
-  createdBy?: string;
-  modifiedByName?: string;
-  createdByName?: string;
-
-  parentId: string;
-  shortName: string;
-  longName: string;
-  type: string;
-  organizationId: string;
-  divisionId: string;
-  missionPriorityId: string;
-  agencyPriority: number;
-  directoratePriority: number;
-  secDefLOEId: string;
-  strategicImperativeId: string;
-  agencyObjectiveId: string;
-  imageName: string;
-  imageArea: string;
+  requestNumber?: string;
+  ufrName?: string;
+  notes?: string;
+  disposition?: Disposition;
+  dispositionExplanation?: string;
+  ufrStatus?: UFRStatus;
+  shortyType?: ShortyType;
+  originatedFrom?: UFR;
 }
--- src/app/programming-feature/services/ufr-service.ts
+++ src/app/programming-feature/services/ufr-service.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 import { BaseRestService } from '../../services/base-rest.service';
 import { HttpClient } from '@angular/common/http';
+import { UFR } from '../models/ufr.model';
 
 export abstract class UfrService extends BaseRestService {
   constructor(protected httpClient: HttpClient) {
@@ -8,6 +9,7 @@ export abstract class UfrService extends BaseRestService {
   }
 
   abstract getByProgramShortName(shortName: string): Observable<object>;
-
   abstract getById(id: string): Observable<object>;
+  abstract getByContainerId(containerId: string): Observable<any>;
+  abstract create(ufr: UFR): Observable<any>;
 }
