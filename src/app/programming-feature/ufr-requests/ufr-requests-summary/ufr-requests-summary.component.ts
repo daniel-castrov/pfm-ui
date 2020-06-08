832add6f9ad577d8bd946b5a9e49fd75565d284a
ariendy <ariendyjav@gmail.com>
2020-06-08 14:07:04 -0400

    Fix navigation history and null value error.

--- src/app/programming-feature/ufr-requests/ufr-requests-summary/ufr-requests-summary.component.ts
+++ src/app/programming-feature/ufr-requests/ufr-requests-summary/ufr-requests-summary.component.ts
@@ -606,8 +606,8 @@ export class UfrRequestsSummaryComponent implements OnInit {
             }
           }),
           switchMap(resp => {
-            const activeProgram = resp.result;
-            if (activeProgram) {
+            if (resp) {
+              const activeProgram = resp.result;
               program = activeProgram;
             }
             return this.mrdbService.getByName(shortName);
@@ -729,7 +729,7 @@ export class UfrRequestsSummaryComponent implements OnInit {
   loadPreviousContainerSelection() {
     const selectedContainer = this.requestSummaryNavigationHistoryService.getSelectedContainer();
     if (selectedContainer) {
-      const listItem = this.poms.find(p => p.id === selectedContainer);
+      const listItem = this.poms.find(p => p.value === selectedContainer);
       listItem.isSelected = true;
       this.onPomChanged(listItem);
     }
