c95c28880f5a81233057c1005c6373a8a2f1d8d3
osirispujols <osirispujols@gmail.com>
2020-06-23 17:37:44 -0400

    Fix mapping issue in Justification Tab.

--- src/app/programming-feature/ufr-requests/ufr-requests-detail/ufr-justification/ufr-justification.component.ts
+++ src/app/programming-feature/ufr-requests/ufr-requests-detail/ufr-justification/ufr-justification.component.ts
@@ -94,7 +94,7 @@ export class UfrJustificationComponent implements OnInit {
     this.pomService
       .getPomById(this.ufr.containerId)
       .pipe(
-        switchMap((resp: any) => {
+        map((resp: any) => {
           const pom = resp.result as Pom;
           if (pom.status === PomStatus.OPEN) {
             return this.workspaceService.getByContainerIdAndVersion(pom.id, 1).pipe(
